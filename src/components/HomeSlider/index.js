db4abe87cbac047aa83f743f5c343cca4d407bc7
p-rk <rama.ped@gmail.com>
2018-03-24 01:26:17 +0530

    small changes

--- src/components/HomeSlider/index.js
+++ src/components/HomeSlider/index.js
@@ -1,56 +1,12 @@
 import React, { Component } from 'react';
-import Slider from 'react-slick';
 
 const MainSliderItem = require('../../data/MainSliderItem.js');
 
 console.log(MainSliderItem);
 
-const settings = {
-  dots: false,
-  infinite: true,
-  speed: 500,
-  arrows: true,
-  slidesToShow: 8,
-  slidesToScroll: 1,
-  autoplaySpeed: false,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    }
-  ]
-};
-
 export default class HomeSlider extends Component {
   render() {
-    const styles = require('./HomeSlider.scss');
-    return (
-      <div className={styles.homeSlider}>
-        <Slider {...settings}>
-          {MainSliderItem.map(slide => (
-            <div key={slide.id} className={`${styles.mainSliderItem}`}>
-              <img src={slide.img} alt="" />
-            </div>
-          ))}
-        </Slider>
-      </div>
-    );
+    // const styles = require('./HomeSlider.scss');
+    return <div>Slider</div>;
   }
 }
