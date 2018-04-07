ca631b76974789cb876c8e4f92c0125b4e4878f1
Ankit Patel <ankitpatel4635@gmail.com>
2018-04-07 17:07:08 +0530

    add product component in listing page

--- src/components/Listing/index.js
+++ src/components/Listing/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Product from 'hometown-components/lib/Product';
+import ProductItems from '../../data/RecentlyViewedProducts.js';
 
 export default class Listing extends Component {
   render() {
-    const styles = require('./Listing.scss');
+    // const styles = require('./Listing.scss');
 
-    return <div className={styles.Listing}>Listing</div>;
+    return <div>{ProductItems.map(item => <Product key={item.id} itemData={item} rating />)}</div>;
   }
 }
