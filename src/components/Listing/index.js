6d2d44cfdb2d21c0107d6c6771d76d47878b13d0
Ankit Patel <ankitpatel4635@gmail.com>
2018-04-10 13:39:22 +0530

    listing heading changes

--- src/components/Listing/index.js
+++ src/components/Listing/index.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import Product from 'hometown-components/lib/Product';
 import Container from 'hometown-components/lib/Container';
 import Div from 'hometown-components/lib/Div';
+import Heading from 'components/Heading';
+import Product from 'hometown-components/lib/Product';
+import Row from 'components/Row';
+import Section from 'components/Section';
 import ProductItems from '../../data/RecentlyViewedProducts.js';
 
 export default class Listing extends Component {
@@ -10,7 +13,18 @@ export default class Listing extends Component {
 
     return (
       <Container>
-        <Div>{ProductItems.map(item => <Product key={item.id} itemData={item} rating />)}</Div>
+        <Section>
+          <Row>
+            <Div>
+              <Heading>One Seater Sofas (74)</Heading>
+            </Div>
+          </Row>
+        </Section>
+        <Section>
+          <Row>
+            <Div>{ProductItems.map(item => <Product key={item.id} itemData={item} rating />)}</Div>
+          </Row>
+        </Section>
       </Container>
     );
   }
