07e5cb5a526fe9b42b267b231f71a1aad7faeef0
Ankit Patel <ankitpatel4635@gmail.com>
2018-04-24 18:26:44 +0530

    design login screen

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import LoginForm from 'hometown-components/lib/Forms/LoginForm';
+import Section from 'hometown-components/lib/Section';
+import Row from 'hometown-components/lib/Row';
+import Heading from 'hometown-components/lib/Heading';
+import Div from 'hometown-components/lib/Div';
+import Link from 'hometown-components/lib/Link';
+import { Label } from 'hometown-components/lib/Label';
+import Img from 'hometown-components/lib/Img';
 import { validateEmail, isBlank } from 'js-utility-functions';
+
+const closeIcon = require('../../../static/closebutton.png');
 
 export default class LoginFormContainer extends Component {
   constructor() {
@@ -45,24 +54,66 @@ export default class LoginFormContainer extends Component {
         passwordErrorMessage: checkPassword ? "Password can't be blank" : ''
       });
     }
-    console.log(this.state);
+    // console.log(this.state);
   };
   render() {
+    const styles = require('./index.scss');
+
     const {
       email, password, emailError, emailErrorMessage, passwordError, passwordErrorMessage
     } = this.state;
     return (
-      <LoginForm
-        email={email}
-        onChangeEmail={this.onChangeEmail}
-        emailFeedBackError={emailError}
-        emailFeedBackMessage={emailErrorMessage}
-        password={password}
-        onChangePassword={this.onChangePassword}
-        passwordFeedBackError={passwordError}
-        passwordFeedBackMessage={passwordErrorMessage}
-        onSubmitLogin={this.onSubmitLogin}
-      />
+      <div className={styles.loginWrapper}>
+        <Section p="0" mb="0.3125rem">
+          <div className={styles.imgWrapper}>
+            <Img src="http://via.placeholder.com/720x480" />
+            <div className={styles.back}>
+              <img src={closeIcon} alt="Back" />
+            </div>
+          </div>
+        </Section>
+        <Section mb="0" p="1.25rem" pt="1.5rem" pb="1.5rem">
+          <Row display="block" mr="0" ml="0">
+            <Div col="6">
+              <Heading mt="0" mb="0" color="textDark">
+                Login
+              </Heading>
+            </Div>
+            <Div col="6" ta="right">
+              <Label fontFamily="light">
+                <Link href="#login">New User? Sign Up now</Link>
+              </Label>
+            </Div>
+          </Row>
+          <Row display="block" mr="0" ml="0">
+            <Div mt="1.25rem">
+              <LoginForm
+                email={email}
+                onChangeEmail={this.onChangeEmail}
+                emailFeedBackError={emailError}
+                emailFeedBackMessage={emailErrorMessage}
+                password={password}
+                onChangePassword={this.onChangePassword}
+                passwordFeedBackError={passwordError}
+                passwordFeedBackMessage={passwordErrorMessage}
+                onSubmitLogin={this.onSubmitLogin}
+              />
+            </Div>
+          </Row>
+          <Row display="block" mr="0" ml="0" pt="1.5625rem">
+            <Div col="6">
+              <Label fontFamily="light">
+                <Link href="#forgot">Forgot Password?</Link>
+              </Label>
+            </Div>
+            <Div col="6" ta="right">
+              <Label fontFamily="light">
+                <Link href="#otp">Login via OTP?</Link>
+              </Label>
+            </Div>
+          </Row>
+        </Section>
+      </div>
     );
   }
 }
