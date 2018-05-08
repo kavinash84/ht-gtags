a101866764cfd393d925866efe627077aefc9380
Ankit Patel <ankitpatel4635@gmail.com>
2018-05-08 18:18:00 +0530

    add heart and cart icon in menu

--- src/components/Menu/MenuSidebar.js
+++ src/components/Menu/MenuSidebar.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Img from 'hometown-components/lib/Img';
+import Div from 'hometown-components/lib/Div';
+import Button from 'hometown-components/lib/Buttons';
+import { HOME_URL } from 'helpers/Constants';
 
 const LogoutIcon = require('../../../static/logout.jpg');
+const LogoIcon = require('../../../static/logo.png');
+const CartIcon = require('../../../static/cart.jpg');
 
 export default class MenuSidebar extends Component {
   state = {
@@ -16,19 +23,32 @@ export default class MenuSidebar extends Component {
   render() {
     const { open } = this.state;
     const styles = require('./MenuSidebar.scss');
-    // const userImg = require('http://via.placeholder.com/60x60');
-    // const closeIcon = require('http://via.placeholder.com/17x17');
 
     return (
       <div>
         <div className={styles.hamburger}>
-          <div className={styles.barContainer}>
-            <button className="noPadding" onClick={this.onClick}>
-              <div className={styles.bar1} />
-              <div className={styles.bar2} />
-              <div className={styles.bar3} />
-            </button>
-          </div>
+          <Div col="7">
+            <div className={styles.barContainer}>
+              <button className="noPadding" onClick={this.onClick}>
+                <div className={styles.bar1} />
+                <div className={styles.bar2} />
+                <div className={styles.bar3} />
+              </button>
+            </div>
+            <div className={styles.logoWrapper}>
+              <Link to={HOME_URL}>
+                <Img src={LogoIcon} alt="Hometown" />
+              </Link>
+            </div>
+          </Div>
+          <Div col="5" ta="right">
+            <Button p="0" className={styles.heartBtn}>
+              <div className="heart-shape" />
+            </Button>
+            <Link className={styles.cart} to={HOME_URL}>
+              <Img src={CartIcon} alt="Hometown" />
+            </Link>
+          </Div>
         </div>
         <div className={`${styles.sidebar} ${open ? styles.show : ''}`}>
           <div className={styles.sidebarContainer}>
