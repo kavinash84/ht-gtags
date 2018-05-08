af895d2739b2f97a8f7a8dda58d31919dc440169
p-rk <rama.ped@gmail.com>
2018-05-08 12:01:48 +0530

    changes in service worker

--- src/client.js
+++ src/client.js
@@ -111,16 +111,40 @@ const providers = { app: {}, restApp: {}, client };
   }
 
   if (online && !__DEVELOPMENT__ && 'serviceWorker' in navigator) {
-    window.addEventListener('load', async () => {
-      try {
-        await navigator.serviceWorker.register('/dist/service-worker.js', { scope: '/' });
-        console.log('Service worker registered!');
-      } catch (error) {
-        console.log('Error registering service worker: ', error);
-      }
+    try {
+      const registration = await navigator.serviceWorker.register('/dist/service-worker.js', { scope: '/' });
+      registration.onupdatefound = () => {
+        // The updatefound event implies that reg.installing is set; see
+        // https://w3c.github.io/ServiceWorker/#service-worker-registration-updatefound-event
+        const installingWorker = registration.installing;
 
-      await navigator.serviceWorker.ready;
-      console.log('Service Worker Ready');
-    });
+        installingWorker.onstatechange = () => {
+          switch (installingWorker.state) {
+            case 'installed':
+              if (navigator.serviceWorker.controller) {
+                // At this point, the old content will have been purged and the fresh content will
+                // have been added to the cache.
+                // It's the perfect time to display a "New content is available; please refresh."
+                // message in the page's interface.
+                console.log('New or updated content is available.');
+              } else {
+                // At this point, everything has been precached.
+                // It's the perfect time to display a "Content is cached for offline use." message.
+                console.log('Content is now available offline!');
+              }
+              break;
+            case 'redundant':
+              console.error('The installing service worker became redundant.');
+              break;
+            default:
+          }
+        };
+      };
+    } catch (error) {
+      console.log('Error registering service worker: ', error);
+    }
+
+    await navigator.serviceWorker.ready;
+    console.log('Service Worker Ready');
   }
 })();
