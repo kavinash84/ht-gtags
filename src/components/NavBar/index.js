b6502239518c8d38e30384e1986f7fc26eaf9c62
Ankit Patel <ankitpatel4635@gmail.com>
2018-05-09 11:16:49 +0530

    menu slider remove and add css for scroll

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Carousel from '../Carousel';
+// import Carousel from '../Carousel';
 
 const styles = require('./NavBar.scss');
 const menuItems = require('../../data/Menu.js');
@@ -7,20 +7,11 @@ const menuItems = require('../../data/Menu.js');
 const NavBar = () => (
   <div className={styles.navBar}>
     <div className={styles.navBarSlider}>
-      <Carousel
-        autoPlayVal={false}
-        className="menuSlider"
-        showThumbsVal={false}
-        showStatusVal={false}
-        showArrowsVal={false}
-        showIndicatorsVal={false}
-        infiniteLoopVal={false}
-        centerModeVal
-        centerSlidePercentageVal={42}
-        sliderImages={menuItems}
-        contentStatus
-        typeOfSlider="menuSlider"
-      />
+      {menuItems.map(menuItem => (
+        <a href="#home" key={menuItem.id}>
+          {menuItem.name}
+        </a>
+      ))}
     </div>
   </div>
 );
