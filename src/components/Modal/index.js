2ec62ca107f85f8aea83690c75d69ad1795d9554
p-rk <rama.ped@gmail.com>
2018-06-05 16:56:09 +0530

    adding pincode popup

---
+++ src/components/Modal/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Modal from 'react-responsive-modal';
+
+const ResponsiveModal = ({ open, onCloseModal, children }) => (
+  <Modal open={open} onClose={onCloseModal} center>
+    {children}
+  </Modal>
+);
+
+ResponsiveModal.defaultProps = {
+  open: false
+};
+
+ResponsiveModal.propTypes = {
+  open: PropTypes.bool,
+  onCloseModal: PropTypes.func.isRequired,
+  children: PropTypes.objectOf(PropTypes.any).isRequired
+};
+
+export default ResponsiveModal;
--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Img from 'hometown-components/lib/Img';
 import Div from 'hometown-components/lib/Div';
 import Button from 'hometown-components/lib/Buttons';
 import Span from 'hometown-components/lib/Span';
 import Search from 'components/Search';
+import ResponsiveModal from 'components/Modal';
+import Pincode from 'components/Pincode';
 import { HOME_URL } from 'helpers/Constants';
 
 const LogoIcon = require('../../../static/logo.png');
@@ -13,10 +17,23 @@ const MapIcon = require('../../../static/map-icon.svg');
 const PhoneIcon = require('../../../static/phone-icon.svg');
 const FavIcon = require('../../../static/fav-icon.svg');
 
+@connect(({ pincode }) => ({
+  selectedPincode: pincode.selectedPincode
+}))
 export default class MenuSidebar extends Component {
+  state = {
+    open: false
+  };
+  onOpenModal = () => {
+    this.setState({ open: true });
+  };
+
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
   render() {
     const styles = require('./TopBar.scss');
-
+    const { selectedPincode } = this.props;
     return (
       <div>
         <div className={styles.hamburger}>
@@ -31,10 +48,13 @@ export default class MenuSidebar extends Component {
             <Search />
           </Div>
           <Div col="5" ta="right" pt="0.625rem">
-            <Link className={styles.cart} to={HOME_URL}>
+            <button onClick={this.onOpenModal}>
               <Img src={MapIcon} alt="Hometown" height="26px" mr="0.625rem" float="left" />
-              <Span fontSize="0.875em">400076</Span>
-            </Link>
+              <Span fontSize="0.875rem">{selectedPincode !== '' ? selectedPincode : 'Pincode'}</Span>
+            </button>
+            <ResponsiveModal onCloseModal={this.onCloseModal} open={this.state.open}>
+              <Pincode />
+            </ResponsiveModal>
             <Link className={styles.cart} to={HOME_URL}>
               <Img src={PhoneIcon} alt="Hometown" height="26px" mr="0.625rem" float="left" />
               <Span fontSize="0.875em">1800-210-0004</Span>
@@ -53,3 +73,11 @@ export default class MenuSidebar extends Component {
     );
   }
 }
+
+MenuSidebar.defaultProps = {
+  selectedPincode: ''
+};
+
+MenuSidebar.propTypes = {
+  selectedPincode: PropTypes.string
+};
