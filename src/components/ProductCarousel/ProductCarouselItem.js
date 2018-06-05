0a4c3859752e5a0ce8eee4a80477562f798ed585
Ankit Patel <ankitpatel4635@gmail.com>
2018-06-06 00:01:16 +0530

    add/design prod carousel

---
+++ src/components/ProductCarousel/ProductCarouselItem.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Div from 'hometown-components/lib/Div';
+import { Link } from 'react-router-dom';
+
+const styles = require('./Slider.scss');
+
+const ProductItem = ({
+  image, name, url, discPrice, price, percentage, rating, saving
+}) => (
+  <Div className={styles.prodSliderItem}>
+    <Link className={styles.link} to={url}>
+      <img src={image} alt={name} />
+      <div className={styles.content}>
+        <p className={styles.title}>{name}</p>
+        <div>
+          <div className={`${styles.priceWrapper} taLeft`}>
+            <span className={styles.discPrice}>Rs. {discPrice}</span>
+            <span className={styles.price}>Rs. {price}</span>
+            <span className={styles.saving}>
+              Saving Rs. {saving} ({percentage})
+            </span>
+          </div>
+          <div className={styles.rating}>{rating}</div>
+        </div>
+      </div>
+    </Link>
+  </Div>
+);
+
+ProductItem.defaultProps = {
+  image: ''
+};
+
+ProductItem.propTypes = {
+  image: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  discPrice: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  percentage: PropTypes.string.isRequired,
+  rating: PropTypes.string.isRequired,
+  saving: PropTypes.string.isRequired
+};
+
+export default ProductItem;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -5,13 +5,14 @@ import Helmet from 'react-helmet';
 import Menu from 'containers/MenuNew/index';
 import MainSlider from 'components/MainSlider';
 import CategoryCarousel from 'components/CategoryCarousel';
+import ProductCarousel from 'components/ProductCarousel';
 import Section from 'hometown-components/lib/Section';
 import { connect } from 'react-redux';
-import ProductSlider from 'components/ProductSlider';
 import HashTags from 'components/Home/HashTags';
 import Stores from 'components/Home/Stores';
 import Footer from 'components/Footer';
 
+const prodSliderItem = require('../../data/RecentlyViewedProducts.js');
 const storesItems = require('../../data/stores.js');
 
 @connect(({ homepage }) => ({
@@ -37,8 +38,7 @@ export default class Home extends Component {
             />
           ))}
           <HashTags />
-          <ProductSlider productSliderTitle="Recommended for you" colSize={20} />
-          <ProductSlider productSliderTitle="Top Selling Products" colSize={20} />
+          <ProductCarousel data={prodSliderItem} title="Recommended for you" />
           <Stores
             categoryName="Visit Our Stores"
             subTitle="Explore all products in a store near you, explore them here, there, anywhere"
