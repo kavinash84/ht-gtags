be8b68fab59048994443de10effff1cc3df81e56
p-rk <rama.ped@gmail.com>
2018-06-06 08:20:15 +0530

    changes in displaying data

--- src/components/ProductCarousel/ProductCarouselItem.js
+++ src/components/ProductCarousel/ProductCarouselItem.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const styles = require('./Slider.scss');
 
 const ProductItem = ({
-  image, name, url, discPrice, price, percentage, rating, saving
+  image, name, url, discPrice, price, rating, saving
 }) => (
   <Div className={styles.prodSliderItem}>
     <Link className={styles.link} to={url}>
@@ -15,11 +15,9 @@ const ProductItem = ({
         <p className={styles.title}>{name}</p>
         <div>
           <div className={`${styles.priceWrapper} taLeft`}>
-            <span className={styles.discPrice}>Rs. {discPrice}</span>
+            <span className={styles.discPrice}>{discPrice}</span>
             <span className={styles.price}>{price}</span>
-            <span className={styles.saving}>
-              Saving {saving} ({percentage})
-            </span>
+            <span className={styles.saving}>Saving ({saving.replace('-', '')})</span>
           </div>
           <div className={styles.rating}>{rating}</div>
         </div>
@@ -39,7 +37,6 @@ ProductItem.propTypes = {
   url: PropTypes.string.isRequired,
   discPrice: PropTypes.string.isRequired,
   price: PropTypes.string.isRequired,
-  percentage: PropTypes.string.isRequired,
   rating: PropTypes.number.isRequired,
   saving: PropTypes.string.isRequired
 };
