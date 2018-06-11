84cb398f097e5c1baac0b239212314d6bf68acbf
Ankit Patel <ankitpatel4635@gmail.com>
2018-06-11 13:07:19 +0530

    add account icon in menu and design login/signup popover

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Img from 'hometown-components/lib/Img';
 import Div from 'hometown-components/lib/Div';
+import Row from 'hometown-components/lib/Row';
 import Button from 'hometown-components/lib/Buttons';
 import Span from 'hometown-components/lib/Span';
+import { Label } from 'hometown-components/lib/Label';
 import Search from 'components/Search';
 import ResponsiveModal from 'components/Modal';
 import Pincode from 'components/Pincode';
@@ -16,6 +18,7 @@ const CartIcon = require('../../../static/cart-icon.svg');
 const MapIcon = require('../../../static/map-icon.svg');
 const PhoneIcon = require('../../../static/phone-icon.svg');
 const FavIcon = require('../../../static/fav-icon.svg');
+const UserIcon = require('../../../static/user-icon.svg');
 
 @connect(({ pincode }) => ({
   selectedPincode: pincode.selectedPincode
@@ -49,7 +52,7 @@ export default class MenuSidebar extends Component {
           </Div>
           <Div col="5" ta="right" pt="0.625rem">
             <button onClick={this.onOpenModal}>
-              <Img src={MapIcon} alt="Hometown" height="26px" mr="0.625rem" float="left" />
+              <Img src={MapIcon} alt="Hometown" height="24px" mr="0.625rem" float="left" />
               <Span fontSize="0.875rem">{selectedPincode !== '' ? selectedPincode : 'Pincode'}</Span>
             </button>
             <ResponsiveModal
@@ -60,17 +63,39 @@ export default class MenuSidebar extends Component {
               <Pincode onCloseModal={this.onCloseModal} />
             </ResponsiveModal>
             <Link className={styles.cart} to={HOME_URL}>
-              <Img src={PhoneIcon} alt="Hometown" height="26px" mr="0.625rem" float="left" />
+              <Img src={PhoneIcon} alt="Hometown" height="24px" mr="0.625rem" float="left" />
               <Span fontSize="0.875em">1800-210-0004</Span>
             </Link>
-            <Button p="0" m className={styles.heartBtn} ml="1.25rem">
-              <Img src={FavIcon} alt="Wishlist" height="26px" mr="0.625rem" float="left" />
+            <Button p="0" className={styles.heartBtn} ml="1.25rem">
+              <Img src={FavIcon} alt="Wishlist" height="24px" mr="0.625rem" float="left" />
               <span className={styles.count}>0</span>
             </Button>
             <Link className={styles.cart} to={HOME_URL}>
-              <Img src={CartIcon} alt="Hometown" height="26px" />
+              <Img src={CartIcon} alt="Hometown" height="24px" />
               <span className={styles.count}>0</span>
             </Link>
+            <Button p="0" className={styles.heartBtn} ml="1.25rem">
+              <Img src={UserIcon} alt="Account" height="24px" mr="0.625rem" float="left" />
+            </Button>
+            <div className={`${styles.yourAccount} ${styles.hide}`}>
+              <Row display="block" mr="0" ml="0">
+                <Div col="12">
+                  <Label fontFamily="regular" fontSize="1rem" mb="0.75rem" mt="0" color="black">
+                    Your Account
+                  </Label>
+                </Div>
+              </Row>
+              <Row display="block" mr="0" ml="0">
+                <div className={styles.yourAccountWrapper}>
+                  <Div col="6" pr="5px">
+                    <Link to={HOME_URL}>Sign Up</Link>
+                  </Div>
+                  <Div col="6" pl="5px">
+                    <Link to={HOME_URL}>Log In</Link>
+                  </Div>
+                </div>
+              </Row>
+            </div>
           </Div>
         </div>
       </div>
