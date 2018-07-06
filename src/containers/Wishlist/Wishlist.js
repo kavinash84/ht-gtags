576adcc029b0028928d5ab228c257b5e6633c0ce
ankit <ankitpatel4635@gmail.com>
2018-07-06 12:13:11 +0530

    fix wishlist design issue

--- src/containers/Wishlist/Wishlist.js
+++ src/containers/Wishlist/Wishlist.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Wishlist from 'components/Wishlist';
-import Row from 'hometown-components/lib/Row';
 import Div from 'hometown-components/lib/Div';
 import Container from 'hometown-components/lib/Container';
 import Empty from 'hometown-components/lib/Empty';
 import Img from 'hometown-components/lib/Img';
 import Section from 'hometown-components/lib/Section';
 import Menu from 'containers/MenuNew/index';
+import Footer from 'components/Footer';
 import MyMenu from 'components/MyMenu';
 import { getSKUList } from '../../selectors/wishlist';
 
@@ -30,18 +30,18 @@ export default class WishlistContainer extends Component {
     wishlistKey: ''
   };
   render() {
-    const { wishlist: { data }, wishListedSKUs, wishlistKey } = this.props;
+    const {
+      wishlist: { data },
+      wishListedSKUs,
+      wishlistKey
+    } = this.props;
     return (
       <Div>
         <Menu />
         <MyMenu page="wishlist" />
         {data && data.length ? (
-          <Container type="container" pr="1rem" pl="1rem">
-            <Row display="block" mr="0" ml="0" p="2rem 0">
-              <Div col="12">
-                <Wishlist list={data} wishList={wishListedSKUs} wishlistKey={wishlistKey} />
-              </Div>
-            </Row>
+          <Container type="container" pr="0" pl="0">
+            <Wishlist list={data} wishList={wishListedSKUs} wishlistKey={wishlistKey} />
           </Container>
         ) : (
           <Section display="flex" p="0.625rem" pt="1.25rem" mb="0">
@@ -50,6 +50,7 @@ export default class WishlistContainer extends Component {
             </Empty>
           </Section>
         )}
+        <Footer />
       </Div>
     );
   }
