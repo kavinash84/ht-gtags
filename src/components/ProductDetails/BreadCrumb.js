0dba903f6f7f8c16e33578ce5a68c60bed518949
ankit <ankitpatel4635@gmail.com>
2018-07-11 15:41:06 +0530

    add image hover in pdp images

--- src/components/ProductDetails/BreadCrumb.js
+++ src/components/ProductDetails/BreadCrumb.js
@@ -4,18 +4,28 @@ import { Link } from 'react-router-dom';
 
 const styles = require('./BreadCrumb.scss');
 
+const checkBreadCrumbs = data => {
+  if (data.length) {
+    if (data[0]) {
+      return true;
+    }
+  }
+  return false;
+};
+
 const BreadCrumb = ({ categoryDetails }) => {
   let link = '';
   return (
     <ul className={styles.breadCrumbList}>
-      {categoryDetails.map(item => {
-        link += `/${item.url_key}`;
-        return (
-          <li key={item.id}>
-            <Link to={`${link}`}>{item.name}</Link>
-          </li>
-        );
-      })}
+      {checkBreadCrumbs(categoryDetails) &&
+        categoryDetails.map(item => {
+          link += `/${item.url_key}`;
+          return (
+            <li key={item.id}>
+              <Link to={`${link}`}>{item.name}</Link>
+            </li>
+          );
+        })}
     </ul>
   );
 };
--- src/components/ProductDetails/CarouselItem.js
+++ src/components/ProductDetails/CarouselItem.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Div from 'hometown-components/lib/Div';
+import ReactImageZoom from 'react-image-zoom';
 
 const styles = require('./Carousel.scss');
 
-const CategoryItem = ({ image, name }) => (
+const props = {
+  width: 279,
+  scale: 1.5,
+  offset: { vertical: 0, horizontal: 10 },
+  zoomStyle: 'left: 103%;padding: 5px;box-shadow: 2px 2px 10px 0 rgba(0, 0, 0, 0.1);z-index: 1;',
+  zoomLensStyle: 'opacity: 0.5;background-color: gray;'
+};
+
+const CategoryItem = ({ image }) => (
   <Div className={styles.pdSliderItem}>
-    <img src={image} alt={name} />
+    <ReactImageZoom {...props} img={image} />
   </Div>
 );
 
