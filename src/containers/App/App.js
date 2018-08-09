65828f9f186f08dbac2bc5c35648d43b68a2eb97
p-rk <rama.ped@gmail.com>
2018-08-09 14:06:39 +0530

    error handling

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -26,31 +26,26 @@ import Theme from 'hometown-components/lib/Theme';
 
 @provideHooks({
   fetch: async ({ store: { dispatch, getState } }) => {
-    const {
-      pincode: { selectedPincode },
-      app: { sessionId, csrfToken }
-    } = getState();
+    const { pincode: { selectedPincode }, app: { sessionId, csrfToken } } = getState();
     const defaultPincode = selectedPincode === '' ? PINCODE : selectedPincode;
     if (!isSessionSet(getState()) || !sessionId || !csrfToken) {
       await dispatch(generateSession(defaultPincode)).catch(error => console.log(error));
     }
+    if (!isSectionLoaded(getState(), 'menu')) {
+      await wrapDispatch(dispatch, 'menu')(loadMainMenu());
+    }
     if (!isSectionLoaded(getState(), 'banners')) {
-      await wrapDispatch(dispatch, 'banners')(loadBanners()).catch(() => null);
+      await wrapDispatch(dispatch, 'banners')(loadBanners()).catch(error => error);
     }
     if (!isSectionLoaded(getState(), 'categories')) {
-      await wrapDispatch(dispatch, 'categories')(loadCategories()).catch(error => console.log(error));
-    }
-    if (!isSectionLoaded(getState(), 'menu')) {
-      await wrapDispatch(dispatch, 'menu')(loadMainMenu()).catch(error => console.log(error));
+      await wrapDispatch(dispatch, 'categories')(loadCategories()).catch(error => error);
     }
     if (sessionId && !isCartLoaded(getState())) {
-      await dispatch(loadCart(sessionId, defaultPincode)).catch(error => console.log(error));
+      await dispatch(loadCart(sessionId, defaultPincode)).catch(error => error);
     }
   },
   defer: ({ store: { dispatch, getState } }) => {
-    const {
-      userLogin: { isLoggedIn }
-    } = getState();
+    const { userLogin: { isLoggedIn } } = getState();
     if (isLoggedIn && !isWishListLoaded(getState())) {
       dispatch(loadWishlist()).catch(error => console.log(error));
     }
@@ -124,19 +119,12 @@ export default class App extends Component {
 
   componentWillReceiveProps(nextProps) {
     const { dispatch } = this.context.store;
-    const {
-      login: { isLoggedIn },
-      pincode: { selectedPincode },
-      app: { sessionId }
-    } = this.props;
+    const { login: { isLoggedIn }, pincode: { selectedPincode }, app: { sessionId } } = this.props;
     const pincode = selectedPincode === '' ? PINCODE : '';
     if (nextProps.signUp && nextProps.signUp.loaded) {
       const { signUp } = nextProps;
       if (!isLoggedIn && signUp.response.signup_complete) {
-        const {
-          signUp: { response },
-          loginUser
-        } = nextProps;
+        const { signUp: { response }, loginUser } = nextProps;
         if (response.signup_complete) {
           dispatch(loginUser(response.token));
           dispatch(synCart(sessionId, pincode));
