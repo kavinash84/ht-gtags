b9c381115895355695a5442ef078c13db8de4c6f
ankit <ankitpatel4635@gmail.com>
2018-08-14 19:33:47 +0530

    fix seo content issue

--- src/components/SeoContent/index.js
+++ src/components/SeoContent/index.js
@@ -1,40 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import Container from 'hometown-components/lib/Container';
+import SeoContentContainer from 'hometown-components/lib/SeoContent';
 import Div from 'hometown-components/lib/Div';
 import Row from 'hometown-components/lib/Row';
 import Section from 'hometown-components/lib/Section';
-
-const SeoContentContainer = styled.div`
-  padding-bottom: 2rem;
-  padding-top: 2rem;
-  font-size: 0.875rem;
-  line-height: 1.7;
-  h1 {
-    margin-top: 1.5rem;
-    margin-bottom: 0.625rem;
-    font-size: 1.5rem;
-  }
-  h2 {
-    margin-top: 1.5rem;
-    margin-bottom: 0.625rem;
-    font-size: 1.25rem;
-    &:first-child {
-      margin-top: 0;
-      font-size: 1.5rem;
-    }
-  }
-  p {
-    margin-top: 0.625rem;
-    margin-bottom: 0.625rem;
-    font-size: 0.875rem;
-    line-height: 1.7;
-  }
-  br {
-    display: none;
-  }
-`;
 
 const SeoContent = ({ children }) => (
   <Div type="block" pb="0" pt="0">
