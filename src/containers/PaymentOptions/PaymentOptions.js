60aeb009fe63a47959ee6224d97a03ae6b8ea7d0
p-rk <rama.ped@gmail.com>
2018-08-23 11:03:48 +0530

    payment options redirection changes

--- src/containers/PaymentOptions/PaymentOptions.js
+++ src/containers/PaymentOptions/PaymentOptions.js
@@ -2,24 +2,32 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { provideHooks } from 'redial';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 import { load, setSelectedGatewayInSession } from 'redux/modules/paymentoptions';
 import { getPaymentOptions } from 'selectors/payments';
+import { getCartList } from 'selectors/cart';
 import PaymentOptions from 'components/Checkout/PaymentOptions';
 
-@connect(({ paymentoptions }) => ({
-  availableOptions: getPaymentOptions(paymentoptions)
+@withRouter
+@connect(({ paymentoptions, cart }) => ({
+  availableOptions: getPaymentOptions(paymentoptions),
+  cart: getCartList(cart)
 }))
 @provideHooks({
   fetch: async ({ store: { dispatch, getState } }) => {
-    const {
-      app: { sessionId }
-    } = getState();
+    const { app: { sessionId } } = getState();
     await dispatch(load(sessionId));
     /* setting default paymentGateway in API */
     dispatch(setSelectedGatewayInSession('CreditCard', sessionId));
   }
 })
 export default class PaymentOptionsContainer extends Component {
+  componentDidMount() {
+    const { cart, history } = this.props;
+    if (cart && cart.length) {
+      history.push('/checkout/delivery-address');
+    }
+  }
   render() {
     const { availableOptions } = this.props;
     return (
@@ -31,9 +39,13 @@ export default class PaymentOptionsContainer extends Component {
 }
 
 PaymentOptionsContainer.defaultProps = {
-  availableOptions: {}
+  availableOptions: {},
+  cart: [],
+  history: {}
 };
 
 PaymentOptionsContainer.propTypes = {
-  availableOptions: PropTypes.object
+  availableOptions: PropTypes.object,
+  history: PropTypes.object,
+  cart: PropTypes.array
 };
