dc6d0c29afda4903fcb1f75e9b692c7f628e2db6
ankit <ankitpatel4635@gmail.com>
2018-08-31 19:16:51 +0530

    design change payment failure page

--- src/components/PaymentFailure/index.js
+++ src/components/PaymentFailure/index.js
@@ -1,48 +1,30 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Container from 'hometown-components/lib/Container';
 import Div from 'hometown-components/lib/Div';
-import Row from 'hometown-components/lib/Row';
 import Section from 'hometown-components/lib/Section';
-import Heading from 'hometown-components/lib/Heading';
 import Img from 'hometown-components/lib/Img';
-import Text from 'hometown-components/lib/Text';
+import Empty from 'hometown-components/lib/Empty';
 import TitleBar from '../TitleBar';
 
 const PaymentFailedIcon = require('../../../static/failed.svg');
 
-const PaymentFailure = ({ orderId }) => (
+const PaymentFailure = () => (
   <Div type="block">
     <TitleBar title="Payment Failed" />
     <Container type="container" pr="0" pl="0">
-      <Section bg="sectionBgDark" display="block" mt="3rem" mb="3rem" p="1.5rem" pt="1.5rem" pb="1.5rem" height="auto">
-        <Row display="block" mr="0" ml="0">
-          <Div col="12">
-            <Img width="4.5rem" mr="1rem" float="left" src={PaymentFailedIcon} alt="Test" />
-            <Heading mt="0">Error During Payment</Heading>
-            {orderId !== '' ? (
-              <Text fontSize="1rem" mb="0">
-                Dear Customer, The Payment for your order no.<b> {orderId} </b>
-                was not successful.
-              </Text>
-            ) : (
-              <Text fontSize="1rem" mb="0">
-                Something went wrong :( Please try later !
-              </Text>
-            )}
-          </Div>
-        </Row>
+      <Section display="flex" p="0.625rem" pt="2.5rem" pb="1.25rem" mb="0">
+        <Empty
+          title="Dear Customer,The Payment for your order was not successful."
+          subTitle="Please try again later !"
+          btnName="Try Again"
+          url="/cart"
+          bg="#fafafa"
+        >
+          <Img width="100px" src={PaymentFailedIcon} m="auto" alt="Error During Payment" />
+        </Empty>
       </Section>
     </Container>
   </Div>
 );
-
-PaymentFailure.defaultProps = {
-  orderId: ''
-};
-
-PaymentFailure.propTypes = {
-  orderId: PropTypes.string
-};
 
 export default PaymentFailure;
