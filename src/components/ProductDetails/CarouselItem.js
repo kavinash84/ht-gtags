1207fa1c07b72c85aa48180ae2e9195a670cbedd
p-rk <rama.ped@gmail.com>
2018-09-03 18:07:20 +0530

    zoom fix and other changes

--- src/components/ProductDetails/CarouselItem.js
+++ src/components/ProductDetails/CarouselItem.js
@@ -1,30 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Div from 'hometown-components/lib/Div';
-// import ReactImageZoom from 'react-image-zoom';
 import ReactImageMagnify from 'react-image-magnify';
 
 const styles = require('./Carousel.scss');
 
-// const props = {
-//   width: 571,
-//   offset: { vertical: 0, horizontal: 0 },
-//   zoomWidth: 600,
-//   zoomStyle:
-//     'left:50%;padding: 0;display: block !important;z-index: 100;
-// box-shadow: 2px 2px 10px 0 rgba(0, 0, 0, 0.1);border: 1px solid #dddddd;',
-//   zoomLensStyle: 'width:60px;height:50px;opacity: 0.5;background-color: gray;z-index:15;'
-// };
+const getLowResolution = url => url.replace('.jpg', '-product_500.jpg');
 
 const CategoryItem = ({ image }) => (
   <Div className={styles.pdSliderItem}>
-    {/* <ReactImageZoom itemProp="image" {...props} img={image} /> */}
     <ReactImageMagnify
       {...{
         smallImage: {
           alt: '',
           isFluidWidth: true,
-          src: image
+          src: getLowResolution(image)
         },
         largeImage: {
           src: image,
@@ -35,10 +25,6 @@ const CategoryItem = ({ image }) => (
         enlargedImageContainerDimensions: {
           width: '100%',
           height: '100%'
-        },
-        enlargedImageStyle: {
-          width: '200%',
-          height: '200%'
         }
       }}
     />
