840fc7637defad08bf0a1051bc62885df64cd08a
p-rk <rama.ped@gmail.com>
2018-09-05 18:16:26 +0530

    adding level 3 for category page

--- src/components/Category/CategoryFilters.js
+++ src/components/Category/CategoryFilters.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Div from 'hometown-components/lib/Div';
 import Row from 'hometown-components/lib/Row';
 import { Link } from 'react-router-dom';
 import { Label } from 'hometown-components/lib/Label';
+
+class CategoryFilterItem extends Component {
+  state = {
+    show: false
+  };
+  handleClick = () => {
+    this.setState({
+      show: !this.state.show
+    });
+  };
+  render() {
+    const { sub } = this.props;
+    const { show } = this.state;
+    return (
+      <li key={sub.id}>
+        <Label color="textLight" mt="0.625rem" mb="0.625rem" display="block">
+          <Link to={`/${sub.url_key}`}>{sub.name}</Link>
+          {sub.children && <button onClick={this.handleClick}>{show ? '+' : '-'}</button>}
+          <ul>
+            {sub.children &&
+              sub.children.map(sub3 => (
+                <li>
+                  <Label key={sub3.id} color="textLight" mt="0.625rem" mb="0.625rem" display="block">
+                    <Link to={`/${sub3.url_key}`}>{sub3.name}</Link>
+                  </Label>
+                </li>
+              ))}
+          </ul>
+        </Label>
+      </li>
+    );
+  }
+}
 
 const CategoryFilters = ({ data }) => (
   <Row display="block" ml="0" mr="0">
@@ -11,12 +44,7 @@ const CategoryFilters = ({ data }) => (
       <Label color="textDark" mb="1rem" fontFamily="medium" display="block">
         Categories
       </Label>
-      {data &&
-        data.filter(menu => menu.visibility === 'on').map(sub => (
-          <Label key={sub.id} color="textLight" mt="0.625rem" mb="0.625rem" display="block">
-            <Link to={`/${sub.url_key}`}>{sub.name}</Link>
-          </Label>
-        ))}
+      <ul>{data && data.filter(menu => menu.visibility === 'on').map(sub => <CategoryFilterItem sub={sub} />)}</ul>
     </Div>
   </Row>
 );
@@ -27,6 +55,14 @@ CategoryFilters.defaultProps = {
 
 CategoryFilters.propTypes = {
   data: PropTypes.array
+};
+
+CategoryFilterItem.defaultProps = {
+  sub: {}
+};
+
+CategoryFilterItem.propTypes = {
+  sub: PropTypes.object
 };
 
 export default CategoryFilters;
