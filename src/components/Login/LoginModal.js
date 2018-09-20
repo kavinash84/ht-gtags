2b74f8de7cbd95162d632455be1333f48b092198
ankit <ankitpatel4635@gmail.com>
2018-09-20 16:03:51 +0530

    fix login modal design

--- src/components/Login/LoginModal.js
+++ src/components/Login/LoginModal.js
@@ -4,9 +4,7 @@ import GoogleLoginBtn from 'components/Login/GoogleLogin';
 import Fav from 'hometown-components/lib/Icons/Fav';
 import Row from 'hometown-components/lib/Row';
 import Div from 'hometown-components/lib/Div';
-import { Link } from 'react-router-dom';
 import { Label } from 'hometown-components/lib/Label';
-import { SIGNUP_URL, FORGOT_PASSWORD_URL } from 'helpers/Constants';
 
 const styles = require('./LoginModal.scss');
 
@@ -20,35 +18,17 @@ const LoginModal = () => (
         </Label>
       </Div>
     </Row>
-    <Row display="block" mt="1.5rem" mr="0" ml="0">
-      <Div col="12" ta="center" mb="0.625rem">
-        <GoogleLoginBtn />
-      </Div>
-      <Div col="12" mb="1" mt="1">
-        <Label fontFamily="regular" display="block" ta="center" color="primary">
-          OR
-        </Label>
-      </Div>
-    </Row>
-    <Row display="block" mr="0" ml="0">
+    <Row display="block" mr="0" ml="0" pb="2rem" pt="2rem">
       <Div>
         <LoginForm />
       </Div>
     </Row>
-    <Row display="block" mr="0" ml="0" pt="0.625rem">
-      <Div col="6">
-        <Link to={FORGOT_PASSWORD_URL}>
-          <Label fontFamily="regular" color="primary">
-            Forgot Password?
-          </Label>
-        </Link>
-      </Div>
-      <Div col="6" ta="right">
-        <Link to={SIGNUP_URL}>
-          <Label fontFamily="regular" color="primary">
-            New User? Sign Up now
-          </Label>
-        </Link>
+    <Row className={styles.socialLogin} display="block" mr="0" ml="0" pt="2.5rem">
+      <Div col="12" ta="center" mb="0.625rem">
+        <Label fontFamily="regular" ta="center" color="color79716c" mr="0.625rem" fontSize="1rem" va="super">
+          Or continue with
+        </Label>
+        <GoogleLoginBtn />
       </Div>
     </Row>
   </div>
