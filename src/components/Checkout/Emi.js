3460ab0293326d4a5f99c19bb3bdff8afbb375cc
Amar <amar.pathak@hometown.in>
2018-09-24 15:13:07 +0530

    SPAR-704 (FIXED), Fixed citibak emi problem

--- src/components/Checkout/Emi.js
+++ src/components/Checkout/Emi.js
@@ -34,101 +34,57 @@ const EMI = ({
               Choose From Preferred Bank
             </Label>
           </Div>
-
-          <BankCard
-            setPaymentDetails={setPaymentDetails}
-            gateway={selectedGateway}
-            detailkey="emiBank"
-            name="citibank"
-            img="https://static.hometown.in/media/cms/BankLOGO/citi.gif"
-            currentSelection={currentSelection}
-          />
-          <BankCard
-            setPaymentDetails={setPaymentDetails}
-            gateway={selectedGateway}
-            detailkey="emiBank"
-            name="hdfc"
-            img="https://static.hometown.in/media/cms/BankLOGO/hdfc.gif"
-            currentSelection={currentSelection}
-          />
-          <BankCard
-            setPaymentDetails={setPaymentDetails}
-            gateway={selectedGateway}
-            detailkey="emiBank"
-            name="hsbc"
-            img="https://static.hometown.in/media/cms/BankLOGO/hsbc.gif"
-            currentSelection={currentSelection}
-          />
-          <BankCard
-            setPaymentDetails={setPaymentDetails}
-            gateway={selectedGateway}
-            detailkey="emiBank"
-            name="icici"
-            img="https://static.hometown.in/media/cms/BankLOGO/icici.gif"
-            currentSelection={currentSelection}
-          />
-          <BankCard
-            setPaymentDetails={setPaymentDetails}
-            gateway={selectedGateway}
-            detailkey="emiBank"
-            name="axis"
-            img="https://static.hometown.in/media/cms/BankLOGO/axis.gif"
-            currentSelection={currentSelection}
-          />
-          <BankCard
-            setPaymentDetails={setPaymentDetails}
-            gateway={selectedGateway}
-            detailkey="emiBank"
-            name="sbi"
-            img="https://static.hometown.in/media/cms/BankLOGO/sbi.gif"
-            currentSelection={currentSelection}
-          />
-          <BankCard
-            setPaymentDetails={setPaymentDetails}
-            gateway={selectedGateway}
-            detailkey="emiBank"
-            name="kotak"
-            img="https://static.hometown.in/media/cms/BankLOGO/kotak.gif"
-            currentSelection={currentSelection}
-          />
+          {emiBankDetails.map(bank => (
+            <BankCard
+              setPaymentDetails={setPaymentDetails}
+              gateway={selectedGateway}
+              detailkey="emiBank"
+              name={bank.bank}
+              img={`https://static.hometown.in/media/cms/BankLOGO/${bank.bank}.gif`}
+              currentSelection={currentSelection}
+              key={bank.bank}
+            />
+          ))}
           {currentBankDetails && (
             <Div col="12" mb="1rem" mt="1rem">
               <table border="1" className={`table table-border ${styles.emiTable}`}>
-                <tr>
-                  <th />
-                  <th>Tenure</th>
-                  <th>Annual Interest Rate</th>
-                  <th>EMI Interest</th>
-                  <th>Total Cost</th>
-                  <th>Monthly Instalments</th>
-                </tr>
-
-                {currentBankDetails.values.map((item, index) => (
-                  <tr key={String(index)}>
-                    <td align="center">
-                      <input
-                        type="radio"
-                        onChange={onChangeDetails(setPaymentDetails, selectedGateway)}
-                        name="emiCode"
-                        value={item.emiCode}
-                      />
-                    </td>
-                    <td>{item.value} Months</td>
-                    <td>{item.interestRate}%</td>
-                    <td>
-                      Rs.
-                      {Math.round(item.emiInterest)}
-                    </td>
-                    <td>
-                      Rs.
-                      {Math.round(item.totalAmount)}
-                    </td>
-                    <td>
-                      Rs.
-                      {Math.round(item.EMI)}
-                    </td>
+                <tbody>
+                  <tr>
+                    <th />
+                    <th>Tenure</th>
+                    <th>Annual Interest Rate</th>
+                    <th>EMI Interest</th>
+                    <th>Total Cost</th>
+                    <th>Monthly Instalments</th>
                   </tr>
-                ))}
+
+                  {currentBankDetails.values.map((item, index) => (
+                    <tr key={String(index)}>
+                      <td align="center">
+                        <input
+                          type="radio"
+                          onChange={onChangeDetails(setPaymentDetails, selectedGateway)}
+                          name="emiCode"
+                          value={item.emiCode}
+                        />
+                      </td>
+                      <td>{item.value} Months</td>
+                      <td>{item.interestRate}%</td>
+                      <td>
+                        Rs.
+                        {Math.round(item.emiInterest)}
+                      </td>
+                      <td>
+                        Rs.
+                        {Math.round(item.totalAmount)}
+                      </td>
+                      <td>
+                        Rs.
+                        {Math.round(item.EMI)}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
               </table>
               <Div col="12" mb="1rem" mt="1rem">
                 <CardForm setPaymentDetails={setPaymentDetails} gateway={selectedGateway} />
