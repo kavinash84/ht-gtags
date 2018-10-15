7ad533ff6f0eb0b3eb254acaf0556c8298ceb786
Amar <amar.pathak@hometown.in>
2018-10-16 00:32:20 +0530

    Nav Bar Modular kitchen link update

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -23,7 +23,7 @@ const NavBar = ({
             {menuItem.name}
           </Link>
         ))}
-        <Link onClick={exitOnClick} to="/modular-kitchens" onMouseEnter={handleEnter('')}>
+        <Link onClick={exitOnClick} to="/modular-kitchens-micro" onMouseEnter={handleEnter('')}>
           Modular Kitchens
         </Link>
         <Link onClick={exitOnClick} to="/design-build" onMouseEnter={handleEnter('')}>
