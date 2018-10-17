b2d1ea3e8b8b16d8434d430d004d90834d92f609
p-rk <rama.ped@gmail.com>
2018-10-17 12:42:27 +0530

    bug fixes and other changes for url redirection

--- src/containers/Signup/Signup.js
+++ src/containers/Signup/Signup.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import { withRouter } from 'react-router';
 import Container from 'hometown-components/lib/Container';
 import Section from 'hometown-components/lib/Section';
 import Div from 'hometown-components/lib/Div';
 import MenuFooter from 'containers/MenuFooter';
 import SignupFormContainer from './SignupForm';
 
-@withRouter
 export default class Signup extends Component {
   render() {
     return (
