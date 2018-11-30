bfd721058c53b15ad171c29100344bd44b043f84
Amar <amar.pathak@hometown.in>
2018-11-30 10:39:43 +0530

    SPAR 608

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -21,7 +21,7 @@ import { SIGNUP_URL, HOME_URL, LOGIN_URL, MY_WISHLIST_URL, MY_PROFILE_URL, CART_
 import { logout } from 'redux/modules/login';
 import { getCartCount } from 'selectors/cart';
 import { getWishListCount } from 'selectors/wishlist';
-import { checkRedirection } from 'utils/helper';
+import { titleCase, checkRedirection } from 'utils/helper';
 
 const LogoIcon = require('../../../static/logo.png');
 const CartIcon = require('../../../static/cart-icon.svg');
@@ -44,13 +44,15 @@ const onClickLogout = dispatcher => e => {
 @withRouter
 @connect(
   ({
-    pincode, userLogin, wishlist, cart, router
+    pincode, userLogin, wishlist, cart, router, profile
   }) => ({
     selectedPincode: pincode.selectedPincode,
     isLoggedIn: userLogin.isLoggedIn,
+    name: profile.data.first_name,
     wishListCount: getWishListCount(wishlist),
     cartCount: getCartCount(cart),
-    router
+    router,
+    profile
   }),
   {
     logoutUser: logout
@@ -103,7 +105,7 @@ export default class MenuSidebar extends Component {
     const styles = require('./TopBar.scss');
     const { userPopOver } = this.state;
     const {
-      selectedPincode, isLoggedIn, history, wishListCount, cartCount, logoutUser
+      selectedPincode, isLoggedIn, history, wishListCount, cartCount, logoutUser, name
     } = this.props;
     return (
       <div>
@@ -208,7 +210,11 @@ export default class MenuSidebar extends Component {
               onMouseOver={this.handleUserPopIn}
               onMouseOut={this.handleUserPopOver}
             >
-              <Img src={UserIcon} alt="Account" height="24px" mr="0" float="left" />
+              {isLoggedIn ? (
+                `Hi ${titleCase(name)} `
+              ) : (
+                <Img src={UserIcon} alt="Account" height="24px" mr="0" float="left" />
+              )}
             </Button>
             <div className={`${styles.yourAccount} ${userPopOver ? '' : styles.hide}`}>
               <Row display="block" mr="0" ml="0">
@@ -257,6 +263,7 @@ MenuSidebar.defaultProps = {
   wishListCount: 0,
   cartCount: 0,
   isLoggedIn: false,
+  name: '',
   history: {},
   router: {},
   logoutUser: () => {}
@@ -269,5 +276,6 @@ MenuSidebar.propTypes = {
   wishListCount: PropTypes.number,
   cartCount: PropTypes.number,
   logoutUser: PropTypes.func,
-  router: PropTypes.object
+  router: PropTypes.object,
+  name: PropTypes.string
 };
