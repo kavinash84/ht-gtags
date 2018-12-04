1eb5910c4e9c3ec31d4f7eedc2373da8ea0c0173
Amar <amar.pathak@hometown.in>
2018-12-04 10:38:43 +0530

    Coupon List

--- src/components/Checkout/Coupon.js
+++ src/components/Checkout/Coupon.js
@@ -6,9 +6,10 @@ import Div from 'hometown-components/lib/Div';
 import Button from 'hometown-components/lib/Buttons';
 import Img from 'hometown-components/lib/Img';
 import Span from 'hometown-components/lib/Span';
+import { Label } from 'hometown-components/lib/Label';
 import Theme from 'hometown-components/lib/Theme';
 import LocalInlineNotification from 'components/LocalInlineNotification';
-import { applyCoupon, removeCoupon } from 'redux/modules/coupon';
+import { applyCoupon, removeCoupon, loadCoupons } from 'redux/modules/coupon';
 import { formatAmount } from 'utils/formatters';
 import Notifs from '../../components/Notifs';
 
@@ -33,15 +34,15 @@ class Coupon extends React.Component {
   state = {
     coupon: ''
   };
-
-  handleChange = e => {
-    this.setState({
-      coupon: e.target.value
-    });
-  };
-
+  componentDidMount() {
+    const { pincode, sessionId } = this.props;
+    const { dispatch } = this.context.store;
+    dispatch(loadCoupons(sessionId, pincode));
+  }
   handleApply = e => {
-    e.preventDefault();
+    if (e) {
+      e.preventDefault();
+    }
     const { pincode, sessionId } = this.props;
     const { dispatch } = this.context.store;
     dispatch(applyCoupon(this.state.coupon, sessionId, pincode));
@@ -52,12 +53,26 @@ class Coupon extends React.Component {
     const { dispatch } = this.context.store;
     dispatch(removeCoupon(coupon, sessionId, pincode));
   };
+  handleClick = coupon => {
+    this.setState(
+      {
+        coupon
+      },
+      () => this.handleApply()
+    );
+  };
+
+  handleChange = e => {
+    this.setState({
+      coupon: e.target.value
+    });
+  };
 
   render() {
     const {
       cart,
       notifs,
-      coupon: { loading }
+      coupon: { loading, coupons }
     } = this.props;
     const {
       summary: { coupon: appliedCoupon, coupon_discount: couponDiscount }
@@ -129,38 +144,41 @@ class Coupon extends React.Component {
               </div>
             </div>
           )}
-          {/* <Label ta="center" display="block" mt="0.625rem" mb="0.625rem">
-            OR
-          </Label>
-          <div className={`${styles.offerList} ${styles.active}`}>
-            <ul>
-              <li>
-                <div className={styles.couponWrapper}>
-                  <div className={styles.coupon}>
-                    <div className="checkbox">
-                      <input type="radio" id="checkbox" />
-                      <label htmlFor="checkbox" />
-                    </div>
-                    <label htmlFor="checkbox" className={styles.couponCode}>
-                      FURNROOMAA500
-                    </label>
-                    <label htmlFor="checkbox" className={styles.saveRs}>
-                      Save <span>Rs. 383</span>
-                    </label>
-                  </div>
-                  <p htmlFor="checkbox" className={styles.offerDetails}>
-                    Rs.500 off on minimum purchase of Rs.1499.0
-                  </p>
-                  <p htmlFor="checkbox" className={styles.offerDetails}>
-                    valid till 31st december,2018 Details
-                  </p>
-                  <ul className={styles.offerCondition}>
-                    <li>This is special coupon and can be applied on select style only</li>
-                  </ul>
-                </div>
-              </li>
-            </ul>
-          </div> */}
+          {coupons.length > 0 && (
+            <div>
+              <Label ta="center" display="block" mt="1rem" mb="0.9375rem">
+                OR
+              </Label>
+              <div className={`${styles.offerList} ${styles.active}`}>
+                <ul>
+                  {coupons.map(item => (
+                    <button
+                      onClick={() => {
+                        this.handleClick(item.couponCode);
+                      }}
+                    >
+                      <li>
+                        <div className={styles.couponWrapper}>
+                          <div className={styles.coupon}>
+                            <label htmlFor="checkbox" className={styles.couponCode}>
+                              {item.couponCode}
+                            </label>
+                            <label htmlFor="checkbox" className={styles.saveRs}>
+                              Flat <span>{item.discount_percentage} %</span> Off
+                            </label>
+                          </div>
+                          <p htmlFor="checkbox" className={styles.offerDetails}>
+                            {item.description}
+                          </p>
+                          <ul className={styles.offerCondition} />
+                        </div>
+                      </li>
+                    </button>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          )}
         </Div>
       </div>
     );
