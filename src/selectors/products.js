47249f1e762fa6ec3d16d5064f776860a9e0138f
p-rk <rama.ped@gmail.com>
2018-12-05 16:15:10 +0530

    changes for color filter

--- src/selectors/products.js
+++ src/selectors/products.js
@@ -37,9 +37,26 @@ export const getCategoryName = createSelector(
 
 export const filtersArr = createSelector([productMeta], productList => productList.filter || []);
 
+export const getColorFilter = createSelector([filtersArr], filters =>
+  filters.filter(filter => filter.name === 'Color'));
+
+export const colorArr = createSelector(
+  [getColorFilter],
+  list =>
+    list.length > 0
+      ? list.map(attr =>
+        attr.attributes.constructor === Array
+          ? attr
+          : {
+            ...attr,
+            attributes: [...Object.values(attr.attributes)]
+          })
+      : []
+);
+
 export const filtersList = createSelector(
-  [filtersArr],
-  list => list.filter(x => x.attributes && x.attributes.constructor === Array) || []
+  [filtersArr, colorArr],
+  (list, colorFilter) => [...list.filter(x => x.attributes && x.attributes.constructor === Array), ...colorFilter] || []
 );
 
 export const getFilters = createSelector([filtersList], filters =>
