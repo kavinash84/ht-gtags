e53d845638342e5620642a706ba0f4514e394726
ankit <ankitpatel4635@gmail.com>
2018-12-14 17:43:27 +0530

    add category bar slider

--- src/components/Listing/CategoryBar.js
+++ src/components/Listing/CategoryBar.js
@@ -7,39 +7,48 @@ import Img from 'hometown-components/lib/Img';
 import Section from 'hometown-components/lib/Section';
 import Container from 'hometown-components/lib/Container';
 import { Label } from 'hometown-components/lib/Label';
+import SlickSlider from '../SlickSlider';
 
 const styles = require('./CategoryBar.scss');
 
-const CategoryBar = ({ categoryBar, maxItem }) => (
+const adjustSlides = length => ({
+  slidesToShow: length >= 8 ? 8 : length,
+  slidesToScroll: 1,
+  autoplay: false
+});
+
+const CategoryBar = ({ categoryBar }) => (
   <Section mb="0" p="0">
     <Container>
-      <Row justifyContent="center">
-        {categoryBar.map((item, index) => {
-          if (index < maxItem) {
-            return (
-              <Div className={`${styles.categoryBlock} ${styles.active}`} col="14" display="flex" pb="1rem">
-                <Link to={`/${item.url_key}`} key={item.name}>
-                  <Img width="80px" m="auto" src={item.icon_url ? item.icon_url : '/'} alt={item.name} />
-                  <Label mt="0" mb="0" display="block" ta="center">
-                    {item.name}
-                  </Label>
-                </Link>
-              </Div>
-            );
-          }
-          return null;
-        })}
+      <Row justifyContent="center" className="categoryBarCarousel" mt="0" mb="-1rem">
+        <SlickSlider settings={adjustSlides(categoryBar.length)}>
+          {categoryBar.map((item, index) => (
+            <Div
+              key={String(index)}
+              className={`${styles.categoryBlock} ${styles.active}`}
+              col="12"
+              display="flex"
+              pb="0.625rem"
+              pt="0.625rem"
+            >
+              <Link to={`/${item.url_key}`} key={item.name}>
+                <Img width="80px" m="auto" src={item.icon_url ? item.icon_url : '/'} alt={item.name} />
+                <Label mt="0" mb="0" display="block" ta="center">
+                  {item.name}
+                </Label>
+              </Link>
+            </Div>
+          ))}
+        </SlickSlider>
       </Row>
     </Container>
   </Section>
 );
 
 CategoryBar.defaultProps = {
-  categoryBar: [],
-  maxItem: 8 // default
+  categoryBar: []
 };
 CategoryBar.propTypes = {
-  categoryBar: PropTypes.array,
-  maxItem: PropTypes.number
+  categoryBar: PropTypes.array
 };
 export default CategoryBar;
