ee00f524b7e89a580dbfceafec58726217a82382
Amar <amar.pathak@hometown.in>
2018-12-17 20:41:52 +0530

    show_l4 condition added

--- src/components/Listing/CategoryBar.js
+++ src/components/Listing/CategoryBar.js
@@ -26,24 +26,29 @@ const CategoryBar = ({ categoryBar, pathname }) => {
     <Section mb="0" p="0">
       <Container>
         <Row justifyContent="center" className="categoryBarCarousel" mt="0" mb="-1rem">
+          {/*eslint-disable*/}
           <SlickSlider settings={adjustSlides()}>
-            {categoryBar.map((item, index) => (
-              <Div
-                key={String(index)}
-                className={`${styles.categoryBlock} ${pathname === `/${item.url_key}` ? styles.active : ''}`}
-                col="12"
-                display="flex"
-                pb="0.625rem"
-                pt="0.625rem"
-              >
-                <Link to={`/${item.url_key}`} key={item.name}>
-                  <Img width="80px" m="auto" src={item.icon_url ? item.icon_url : '/'} alt={item.name} />
-                  <Label mt="0" mb="0" display="block" ta="center">
-                    {item.name}
-                  </Label>
-                </Link>
-              </Div>
-            ))}
+            {categoryBar.map((item, index) => {
+              if (item.show_l4 === '1') {
+                return (
+                  <Div
+                    key={String(index)}
+                    className={`${styles.categoryBlock} ${pathname === `/${item.url_key}` ? styles.active : ''}`}
+                    col="12"
+                    display="flex"
+                    pb="0.625rem"
+                    pt="0.625rem"
+                  >
+                    <Link to={`/${item.url_key}`} key={item.name}>
+                      <Img width="80px" m="auto" src={item.icon_url ? item.icon_url : '/'} alt={item.name} />
+                      <Label mt="0" mb="0" display="block" ta="center">
+                        {item.name}
+                      </Label>
+                    </Link>
+                  </Div>
+                );
+              }
+            })}
           </SlickSlider>
         </Row>
       </Container>
