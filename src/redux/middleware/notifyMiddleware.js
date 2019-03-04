5f390f703d89119cec56bae119e8724b26fe41cc
sanjeev-iitr-uk <sanjeev@hometown.in>
2019-03-04 16:08:53 +0530

    login error msg changed

--- src/redux/middleware/notifyMiddleware.js
+++ src/redux/middleware/notifyMiddleware.js
@@ -126,6 +126,7 @@ export default function userMiddleware() {
           msg:
               (action.error.error_message === 'invalid_grant' && 'Incorrect Email or Password') ||
               action.error.error_message ||
+              action.error.error_description ||
               SOME_ERROR,
           dismissAfter: 4000
         }));
