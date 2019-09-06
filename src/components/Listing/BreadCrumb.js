e81c78a8276bad4f4e7bb3ba3cea609d8fcfcf4f
sanjeev-iitr-uk <sanjeev@hometown.in>
2019-09-06 12:15:32 +0530

    breadcrum link fixes for l4 cat

--- src/components/Listing/BreadCrumb.js
+++ src/components/Listing/BreadCrumb.js
@@ -4,6 +4,23 @@ import { Link } from 'react-router-dom';
 
 const styles = require('./BreadCrumb.scss');
 
+const cleanTail = url => {
+  if (url[url.length - 1] === '/') {
+    return url.substring(0, url.length - 1);
+  }
+  return url;
+};
+const formatLink = url => {
+  const paramLink = url.split('/').filter(z => z !== '');
+  if (paramLink.length >= 4) {
+    paramLink.splice(1, 1);
+  }
+  const newLink = paramLink.join('/');
+  const sanitizedUrl = cleanTail(newLink);
+  const newURL = sanitizedUrl.replace('catalog/', '');
+  return newURL;
+};
+
 const BreadCrumb = ({ categoryDetails }) => {
   let link = '';
   return (
@@ -14,20 +31,22 @@ const BreadCrumb = ({ categoryDetails }) => {
           <meta itemProp="position" content={1} />
         </Link>
       </li>
-      {categoryDetails.filter(details => Object.keys(details).length > 0).map((item, index) => {
-        if (item) {
-          link = `/${item.url_key}`;
-          return (
-            <li key={item.id} itemProp="itemListElement" itemType="http://schema.org/ListItem" itemScope>
-              <Link itemProp="item" to={`${link}`}>
-                <span itemProp="name">{item.name}</span>
-                <meta itemProp="position" content={index + 2} />
-              </Link>
-            </li>
-          );
-        }
-        return null;
-      })}
+      {categoryDetails
+        .filter(details => Object.keys(details).length > 0)
+        .map((item, index) => {
+          if (item) {
+            link = `/${item.url_key}`;
+            return (
+              <li key={item.id} itemProp="itemListElement" itemType="http://schema.org/ListItem" itemScope>
+                <Link itemProp="item" to={`/${formatLink(link)}`}>
+                  <span itemProp="name">{item.name}</span>
+                  <meta itemProp="position" content={index + 2} />
+                </Link>
+              </li>
+            );
+          }
+          return null;
+        })}
     </ul>
   );
 };
