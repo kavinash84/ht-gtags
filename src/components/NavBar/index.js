df1c9b5337f749c9d90cf8faebb273bd586a2055
sanjeev-iitr-uk <sanjeev@hometown.in>
2019-09-21 13:00:09 +0530

    making categories ordering dynamic

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import Container from 'hometown-components/lib/Container';
 import { Link } from 'react-router-dom';
 import Button from 'hometown-components/lib/Buttons';
@@ -9,125 +9,126 @@ const styles = require('./NavBar.scss');
 
 // const { SITE_URL } = process.env;
 
-const NavBar = ({
-  menuItems, handleEnter, handleLeave, exitOnClick
-}) => (
-  <div className={styles.navBar} onMouseLeave={handleLeave}>
-    <Container pr="0" pl="0">
-      <div className={styles.navBarSlider}>
-        {menuItems
-          .filter(menu =>
-            menu.visibility === 'on' &&
-              menu.name !== 'Festive Gifts' &&
-              menu.name !== 'Electronics' &&
-              menu.name !== 'Deals of the Week' &&
-              menu.name !== 'Bath')
-          .map(menuItem => (
+class NavBar extends Component {
+  render() {
+    const {
+      menuItems, handleEnter, handleLeave, exitOnClick
+    } = this.props;
+    const cats = menuItems
+      .filter(menu => menu.visibility === 'on' && menu.sort_order && !Number.isNaN(parseInt(menu.sort_order, 10)))
+      .sort((i1, i2) => {
+        const a = parseInt(i1.sort_order, 10);
+        const b = parseInt(i2.sort_order, 10);
+        return a - b;
+      });
+    const main = cats.length ? cats.slice(0, 7) : [];
+    const more = cats.length && cats.length > 7 ? cats.slice(7) : [];
+    return (
+      <div className={styles.navBar} onMouseLeave={handleLeave}>
+        <Container pr="0" pl="0">
+          <div className={styles.navBarSlider}>
+            {main.map(menuItem => (
+              <Link
+                onClick={exitOnClick}
+                title={menuItem.name}
+                to={`/${menuItem.url_key}`}
+                key={menuItem.id}
+                onMouseEnter={handleEnter(menuItem.id)}
+              >
+                {menuItem.name === 'Hot Deals' ? (
+                  <Fragment>
+                    <Label className={styles.newLabel}>New</Label>
+                    {menuItem.name}
+                  </Fragment>
+                ) : (
+                  menuItem.name
+                )}
+              </Link>
+            ))}
             <Link
+              to="/modular-kitchens"
+              title="Modular Kitchens"
+              target="_blank"
+              rel="noopener noreferrer"
               onClick={exitOnClick}
-              title={menuItem.name}
-              to={`/${menuItem.url_key}`}
-              key={menuItem.id}
-              onMouseEnter={handleEnter(menuItem.id)}
             >
-              {menuItem.name === 'Hot Deals' ? (
-                <Fragment>
-                  <Label className={styles.newLabel}>New</Label>
-                  {menuItem.name}
-                </Fragment>
-              ) : (
-                menuItem.name
-              )}
+              Modular Kitchens
             </Link>
-          ))}
-        <Link
-          to="/modular-kitchens"
-          title="Modular Kitchens"
-          target="_blank"
-          rel="noopener noreferrer"
-          onClick={exitOnClick}
-        >
-          Modular Kitchens
-        </Link>
-        <div className={`${styles.moreDropdownWrapper} dropdownWrapper moreDropdownWrapper`}>
-          <Button
-            btnType="custom"
-            bg="transparent"
-            color="#FFF"
-            border="none"
-            fontSize="0.875rem !important"
-            tt="uppercase"
-            p="10px 15px 10px 0px"
-            lh="2"
-            height="46px"
-            className="moreDropdown"
-            fontFamily="light"
-          >
-            More
-          </Button>
-          <div className="dropDown blockRight">
-            <ul>
-              <li>
-                <Label htmlFor="checkbox" fontSize="0.75em" className="dropdownValue">
-                  <Link onClick={exitOnClick} to="/deals-of-the-week" onMouseEnter={handleEnter('')} title="Bath">
-                    Deals of the Week
-                  </Link>
-                </Label>
-              </li>
-              <li>
-                <Label htmlFor="checkbox" fontSize="0.75em" className="dropdownValue">
-                  <Link onClick={exitOnClick} to="/bath" onMouseEnter={handleEnter('')} title="Bath">
-                    Bath
-                  </Link>
-                </Label>
-              </li>
-              <li>
-                <Label htmlFor="checkbox" fontSize="0.75em" className="dropdownValue">
-                  <Link onClick={exitOnClick} to="/electronics" onMouseEnter={handleEnter('')} title="Electronics">
-                    Electronics
-                  </Link>
-                </Label>
-              </li>
-              <li>
-                <Label htmlFor="checkbox" fontSize="0.75em" className="dropdownValue">
-                  <Link onClick={exitOnClick} to="/bulk-order" onMouseEnter={handleEnter('')} title="Bulk Order">
-                    Bulk Order
-                  </Link>
-                </Label>
-              </li>
-              <li>
-                <Label htmlFor="checkbox" fontSize="0.75em" className="dropdownValue">
-                  <Link
-                    to="/design-build"
-                    title="Design & Build"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    onClick={exitOnClick}
-                  >
-                    Design & Build
-                  </Link>
-                </Label>
-              </li>
-              <li>
-                <Label htmlFor="checkbox" fontSize="0.75em" className="dropdownValue">
-                  <a
-                    href="https://static.hometown.in/media/Institutional+Catalogue.pdf"
-                    title="Festive Catalogue"
-                    download
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Gifting Catalogue
-                  </a>
-                </Label>
-              </li>
-            </ul>
+            <div className={`${styles.moreDropdownWrapper} dropdownWrapper moreDropdownWrapper`}>
+              <Button
+                btnType="custom"
+                bg="transparent"
+                color="#FFF"
+                border="none"
+                fontSize="0.875rem !important"
+                tt="uppercase"
+                p="10px 15px 10px 0px"
+                lh="2"
+                height="46px"
+                className="moreDropdown"
+                fontFamily="light"
+              >
+                More
+              </Button>
+              <div className="dropDown blockRight">
+                <ul>
+                  {more.map(menuItem => (
+                    <li>
+                      <Label htmlFor="checkbox" fontSize="0.75em" className="dropdownValue">
+                        <Link
+                          onClick={exitOnClick}
+                          onMouseEnter={handleEnter('')}
+                          title={menuItem.name}
+                          to={`/${menuItem.url_key}`}
+                          key={menuItem.id}
+                        >
+                          {menuItem.name || 'Hometown'}
+                        </Link>
+                      </Label>
+                    </li>
+                  ))}
+                  <li>
+                    <Label htmlFor="checkbox" fontSize="0.75em" className="dropdownValue">
+                      <Link onClick={exitOnClick} to="/bulk-order" onMouseEnter={handleEnter('')} title="Bulk Order">
+                        Bulk Order
+                      </Link>
+                    </Label>
+                  </li>
+                  <li>
+                    <Label htmlFor="checkbox" fontSize="0.75em" className="dropdownValue">
+                      <Link
+                        to="/design-build"
+                        title="Design & Build"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        onClick={exitOnClick}
+                      >
+                        Design & Build
+                      </Link>
+                    </Label>
+                  </li>
+                  <li>
+                    <Label htmlFor="checkbox" fontSize="0.75em" className="dropdownValue">
+                      <a
+                        href="https://static.hometown.in/media/Institutional+Catalogue.pdf"
+                        title="Festive Catalogue"
+                        download
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Gifting Catalogue
+                      </a>
+                    </Label>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
-        </div>
+        </Container>
       </div>
-    </Container>
-  </div>
-);
+    );
+  }
+}
 
 NavBar.defaultProps = {
   menuItems: []
