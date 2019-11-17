9f9980eadde656c940755985698e0d376e434069
ankit patel <ankit.patel@hometown.in>
2019-11-17 18:53:49 +0530

    created slider, carousel and banner using  rebass components

---
+++ src/newComponents/CategoryCarousel/CategoryCarouselItem.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import BoxHtV1 from 'hometown-components/lib/BoxHtV1';
+import ImageHtV1 from 'hometown-components/lib/ImageHtV1';
+import TextHtV1 from 'hometown-components/lib/TextHtV1';
+
+const CategoryItem = ({ image, name, url }) => {
+  if (url) {
+    return (
+      <BoxHtV1 variant="section.catSliderItem">
+        <Link to={url || '/'}>
+          {image && <ImageHtV1 src={image} alt={name} />}
+          <TextHtV1 variant="text.catSliderTitle" mt={6} mb={6}>
+            {name}
+          </TextHtV1>
+        </Link>
+      </BoxHtV1>
+    );
+  }
+  return (
+    <BoxHtV1 variant="section.catSliderItem">
+      {image && <ImageHtV1 src={image} alt={name} />}
+      <TextHtV1 variant="text.catSliderTitle" mt={6} mb={6}>
+        {name}
+      </TextHtV1>
+    </BoxHtV1>
+  );
+};
+
+CategoryItem.defaultProps = {
+  image: '',
+  name: '',
+  url: ''
+};
+
+CategoryItem.propTypes = {
+  image: PropTypes.string,
+  name: PropTypes.string,
+  url: PropTypes.string
+};
+
+export default CategoryItem;
---
+++ src/newComponents/CategoryCarousel/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import BoxHtV1 from 'hometown-components/lib/BoxHtV1';
+import Title from '../Title';
+import CategoryCarouselItem from './CategoryCarouselItem';
+import SlickSlider from '../SlickSlider';
+
+const adjustSlides = length => ({
+  slidesToShow: length >= 4 ? 4 : length,
+  slidesToScroll: 3,
+  autoplaySpeed: 5000
+});
+
+const OFFER_ID = 5;
+
+export default class CategoryCarousel extends Component {
+  render() {
+    const {
+      data, categoryName, subTitle, id
+    } = this.props;
+    return (
+      <BoxHtV1 variant="section.categorycarousel">
+        <div>
+          {(categoryName !== '' || categoryName !== null) && <Title title={categoryName} subTitle={subTitle} />}
+          <SlickSlider settings={adjustSlides(data.length)}>
+            {data.map(slide => (
+              <div key={slide.category_id}>
+                <CategoryCarouselItem
+                  image={slide.image_url}
+                  name={slide.info.name}
+                  url={OFFER_ID === id || OFFER_ID === parseInt(id, 10) ? '' : slide.info.url_key}
+                />
+              </div>
+            ))}
+          </SlickSlider>
+        </div>
+      </BoxHtV1>
+    );
+  }
+}
+
+CategoryCarousel.defaultProps = {
+  data: [],
+  categoryName: '',
+  subTitle: '',
+  id: ''
+};
+
+CategoryCarousel.propTypes = {
+  data: PropTypes.array,
+  categoryName: PropTypes.string,
+  subTitle: PropTypes.string,
+  id: PropTypes.string
+};
