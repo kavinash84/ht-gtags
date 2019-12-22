6e83dd4565b20d1316da67e6f528d8c8992f3904
rashmi05 <rashmi9730@gmail.com>
2019-12-23 02:13:55 +0530

    faq

--- src/containers/FaqV0/Faq.js
+++ src/containers/FaqV0/Faq.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import FaqContainer from 'newComponents/StaticPages/Faqs';
+import FaqContainer from 'components/StaticPages/Faqs';
 import MenuFooter from 'containers/MenuFooter';
 
 export default () => (
