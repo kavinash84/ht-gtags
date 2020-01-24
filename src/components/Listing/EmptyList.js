d2e03bfbb3266cec982a9224f520e3090bb0a387
sanjeev-iitr-uk <sanjeev@hometown.in>
2020-01-24 15:26:40 +0530

    on sync process 1-old components folder deleted

--- src/components/Listing/EmptyList.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const ProductsNotFound = () => (
-  <div>
-    <p>No products found !</p>
-  </div>
-);
-export default ProductsNotFound;
--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-import Provider from './Provider/Provider';
-import ReduxAsyncConnect from './ReduxAsyncConnect/ReduxAsyncConnect';
-
-export { Provider, ReduxAsyncConnect };
