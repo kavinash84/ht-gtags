453b04e7b89c59e58076d0d1f4e0090bce77f02e
Nikhil Bhagath <nikhil@theretailinsights.com>
2020-06-01 13:52:26 +0530

    payment issue fixed

--- src/containers/PaymentSuccess/PaymentSuccess.js
+++ src/containers/PaymentSuccess/PaymentSuccess.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 /* ====== Components ====== */
 import Box from 'hometown-components-dev/lib/BoxHtV1';
@@ -18,190 +20,169 @@ import Footer from 'components/Footer';
 import Header from 'components/Header';
 import ThankYou from 'newComponents/ThankYou';
 
-const PaymentSuccess = () => (
-  <Wrapper>
-    <Body>
-      {/* Header */}
-      <Header />
+/**
+ * Modules / Utils / Reducers
+ */
+import { formatAmount } from 'utils/formatters';
 
-      {/* Container */}
-      <Container pt={60}>
-        <Box variant="col-10" mx="auto">
-          <Box sx={{ boxShadow: 'profile', border: 'light' }}>
-            <ThankYou />
-          </Box>
-          <Row mx={0} mb={40} mt={60} justifyContent="center">
-            <Text fontFamily="medium" fontSize={28}>
-              Here’s what you ordered
-            </Text>
-          </Row>
-          <Row sx={{ borderBottom: 'heading' }} mx={0} pb={5}>
-            <Box variant="col-6" pl={0}>
-              <Text fontSize={16}>Product Details</Text>
-            </Box>
-            <Box variant="col-2">
-              <Text fontSize={16}>Est. Delivery</Text>
-            </Box>
-            <Box variant="col-2">
-              <Text fontSize={16}>Qty.</Text>
-            </Box>
-            <Box variant="col-2">
-              <Text fontSize={16}>Price</Text>
-            </Box>
-          </Row>
-          <Row py={20} mx={0} alignItems="center" sx={{ position: 'relative', borderBottom: 'light' }}>
-            <Box variant="col-2" pl={0}>
-              <Link to="/">
-                <Image
-                  width={1}
-                  src="https://www.hometown.in/media/product/15/1053/1-top_sel_160.jpg"
-                  alt=""
-                  sx={{ boxShadow: 'productThumb' }}
-                />
-              </Link>
-            </Box>
-            <Box variant="col-4" pl={15}>
-              <Link to="/">
-                <Box mb="10px">
-                  <Heading color="heading" fontSize={16} lineHeight={1.4}>
-                    Love To Travel Golden Masonjars Set Of 2 T & P Glass Mason
-                  </Heading>
+const mapStateToProps = ({ paymentstatus }) => ({
+  cartProducts: paymentstatus.data.cart_products,
+  subTotal: paymentstatus.data.sub_total_amount,
+  discount: paymentstatus.data.discount_coupon_value,
+  totalAmount: paymentstatus.data.net_order_amount
+});
+class PaymentSuccess extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+  componentDidMount = () => {
+    this.groupSimilarProducts();
+  };
+  groupSimilarProducts = () => {
+    const { cartProducts } = this.props;
+    let products = [];
+    cartProducts.forEach(arr => {
+      if (products[arr.sku] && products[arr.sku].sku === arr.sku) {
+        products[arr.sku].qty += 1;
+      } else {
+        products[arr.sku] = {
+          sku: arr.sku,
+          name: arr.name,
+          image: arr.details.image,
+          deliveryTime: arr.delivery_text,
+          qty: arr.qty,
+          price: arr.price,
+          color: arr.color
+        };
+      }
+    });
+    console.log(products);
+    products = Object.values(products);
+    this.setState({ products });
+  };
+  render() {
+    const { subTotal, discount, totalAmount } = this.props;
+    const { products } = this.state;
+    return (
+      <Wrapper>
+        <Body>
+          {/* Header */}
+          <Header />
+
+          {/* Container */}
+          <Container pt={60}>
+            <Box variant="col-10" mx="auto">
+              <Box sx={{ boxShadow: 'profile', border: 'light' }}>
+                <ThankYou />
+              </Box>
+              <Row mx={0} mb={40} mt={60} justifyContent="center">
+                <Text fontFamily="medium" fontSize={28}>
+                  Here’s what you ordered
+                </Text>
+              </Row>
+              <Row sx={{ borderBottom: 'heading' }} mx={0} pb={5}>
+                <Box variant="col-6" pl={0}>
+                  <Text fontSize={16}>Product Details</Text>
                 </Box>
-                <Box mb="15px">
-                  <Text color="#575757">Orange,Pink & Gold</Text>
+                <Box variant="col-2">
+                  <Text fontSize={16}>Est. Delivery</Text>
                 </Box>
-              </Link>
-            </Box>
-            <Box variant="col-2">
-              <Label color="heading" fontSize={18}>
-                10/10/2019
-              </Label>
-            </Box>
-            <Box variant="col-2">
-              <Label color="heading" fontSize={18}>
-                1
-              </Label>
-            </Box>
-            <Box variant="col-2">
-              <Label color="heading" fontSize={18}>
-                ₹ 299
-              </Label>
-            </Box>
-          </Row>
-          <Row py={20} mx={0} alignItems="center" sx={{ position: 'relative', borderBottom: 'light' }}>
-            <Box variant="col-2" pl={0}>
-              <Link to="/">
-                <Image
-                  width={1}
-                  src="https://www.hometown.in/media/product/15/1053/1-top_sel_160.jpg"
-                  alt=""
-                  sx={{ boxShadow: 'productThumb' }}
-                />
-              </Link>
-            </Box>
-            <Box variant="col-4" pl={15}>
-              <Link to="/">
-                <Box mb="10px">
-                  <Heading color="heading" fontSize={16} lineHeight={1.4}>
-                    Love To Travel Golden Masonjars Set Of 2 T & P Glass Mason
-                  </Heading>
+                <Box variant="col-2">
+                  <Text fontSize={16}>Qty.</Text>
                 </Box>
-                <Box mb="15px">
-                  <Text color="#575757">Orange,Pink & Gold</Text>
+                <Box variant="col-2">
+                  <Text fontSize={16}>Price</Text>
                 </Box>
-              </Link>
-            </Box>
-            <Box variant="col-2">
-              <Label color="heading" fontSize={18}>
-                10/10/2019
-              </Label>
-            </Box>
-            <Box variant="col-2">
-              <Label color="heading" fontSize={18}>
-                1
-              </Label>
-            </Box>
-            <Box variant="col-2">
-              <Label color="heading" fontSize={18}>
-                ₹ 299
-              </Label>
-            </Box>
-          </Row>
-          <Row py={20} mx={0} alignItems="center" sx={{ position: 'relative', borderBottom: 'light' }}>
-            <Box variant="col-2" pl={0}>
-              <Link to="/">
-                <Image
-                  width={1}
-                  src="https://www.hometown.in/media/product/15/1053/1-top_sel_160.jpg"
-                  alt=""
-                  sx={{ boxShadow: 'productThumb' }}
-                />
-              </Link>
-            </Box>
-            <Box variant="col-4" pl={15}>
-              <Link to="/">
-                <Box mb="10px">
-                  <Heading color="heading" fontSize={16} lineHeight={1.4}>
-                    Love To Travel Golden Masonjars Set Of 2 T & P Glass Mason
-                  </Heading>
-                </Box>
-                <Box mb="15px">
-                  <Text color="#575757">Orange,Pink & Gold</Text>
-                </Box>
-              </Link>
-            </Box>
-            <Box variant="col-2">
-              <Label color="heading" fontSize={18}>
-                10/10/2019
-              </Label>
-            </Box>
-            <Box variant="col-2">
-              <Label color="heading" fontSize={18}>
-                1
-              </Label>
-            </Box>
-            <Box variant="col-2">
-              <Label color="heading" fontSize={18}>
-                ₹ 299
-              </Label>
-            </Box>
-          </Row>
-          <Row>
-            <Box variant="col-2" />
-            <Box variant="col-9" pt={20} pb={20}>
-              <Flex mb={20} justifyContent="space-between">
-                <Text>Subtotal : </Text>
-                <Text>Rs. 1000</Text>
-              </Flex>
-              <Flex mb={20} justifyContent="space-between">
+              </Row>
+              {products.map(product => (
+                <Row py={20} mx={0} alignItems="center" sx={{ position: 'relative', borderBottom: 'light' }}>
+                  <Box variant="col-2" pl={0}>
+                    <Link to="/">
+                      <Image
+                        width={1}
+                        src={`${product.image}-top_sel_160.jpg`}
+                        alt=""
+                        sx={{ boxShadow: 'productThumb' }}
+                      />
+                    </Link>
+                  </Box>
+                  <Box variant="col-4" pl={15}>
+                    <Link to="/">
+                      <Box mb="10px">
+                        <Heading color="heading" fontSize={16} lineHeight={1.4}>
+                          {product.name}
+                        </Heading>
+                      </Box>
+                      <Box mb="15px">
+                        <Text color="#575757">{product.color}</Text>
+                      </Box>
+                    </Link>
+                  </Box>
+                  <Box variant="col-2">
+                    <Label color="heading" fontSize={18}>
+                      {product.deliveryTime.split('Delivered by ')[1]}
+                    </Label>
+                  </Box>
+                  <Box variant="col-2">
+                    <Label color="heading" fontSize={18}>
+                      {product.qty}
+                    </Label>
+                  </Box>
+                  <Box variant="col-2">
+                    <Label color="heading" fontSize={18}>
+                      ₹ {formatAmount(product.price)}
+                    </Label>
+                  </Box>
+                </Row>
+              ))}
+              <Row>
+                <Box variant="col-2" />
+                <Box variant="col-9" pt={20} pb={20}>
+                  <Flex mb={20} justifyContent="space-between">
+                    <Text>Subtotal : </Text>
+                    <Text>Rs {formatAmount(subTotal)}</Text>
+                  </Flex>
+                  {/* <Flex mb={20} justifyContent="space-between">
                 <Text>Savings : </Text>
                 <Text>Rs. 400</Text>
-              </Flex>
-              <Flex mb={20} justifyContent="space-between">
-                <Text>Discount : </Text>
-                <Text>Rs. 300</Text>
-              </Flex>
-              <Row m="0" py="1em" sx={{ borderTop: 'divider' }}>
-                <Box variant="col-6" p="0">
-                  <Text color="menuItem" fontSize={18} fontFamily="medium">
-                    Total Price :
-                  </Text>
-                </Box>
-                <Box variant="col-6" p="0" textAlign="right">
-                  <Text color="menuItem" fontSize={18} fontFamily="medium">
-                    Rs. 3000
-                  </Text>
+              </Flex> */}
+                  <Flex mb={20} justifyContent="space-between">
+                    <Text>Discount : </Text>
+                    <Text>Rs {formatAmount(discount)}</Text>
+                  </Flex>
+                  <Row m="0" py="1em" sx={{ borderTop: 'divider' }}>
+                    <Box variant="col-6" p="0">
+                      <Text color="menuItem" fontSize={18} fontFamily="medium">
+                        Total Price :
+                      </Text>
+                    </Box>
+                    <Box variant="col-6" p="0" textAlign="right">
+                      <Text color="menuItem" fontSize={18} fontFamily="medium">
+                        Rs {formatAmount(totalAmount)}
+                      </Text>
+                    </Box>
+                  </Row>
                 </Box>
               </Row>
             </Box>
-          </Row>
-        </Box>
-      </Container>
+          </Container>
 
-      {/* Footer */}
-      <Footer />
-    </Body>
-  </Wrapper>
-);
+          {/* Footer */}
+          <Footer />
+        </Body>
+      </Wrapper>
+    );
+  }
+}
 
-export default PaymentSuccess;
+PaymentSuccess.propTypes = {
+  cartProducts: PropTypes.array.isRequired,
+  subTotal: PropTypes.number.isRequired,
+  totalAmount: PropTypes.number.isRequired,
+  discount: PropTypes.number.isRequired
+};
+
+export default connect(mapStateToProps, null)(PaymentSuccess);
