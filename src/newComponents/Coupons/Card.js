9c89ac107cad152c4e0917d77dd749368557bac0
Nikhil Bhagath <nikhil@theretailinsights.com>
2020-08-19 11:42:25 +0530

    PRO2-209, Highlighted things not working in dashboard.(Coupon)

--- src/newComponents/Coupons/Card.js
+++ src/newComponents/Coupons/Card.js
@@ -1,33 +1,81 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 /**
  * Components
  */
 import Box from 'hometown-components-dev/lib/BoxHtV1';
 import Col from 'hometown-components-dev/lib/ColHtV1';
-import Image from 'hometown-components-dev/lib/ImageHtV1';
+// import Image from 'hometown-components-dev/lib/ImageHtV1';
 import Row from 'hometown-components-dev/lib/RowHtV1';
 import Text from 'hometown-components-dev/lib/TextHtV1';
 
-const offerIcon = require('../../../static/offer.svg');
+// const offerIcon = require('../../../static/offer.svg');
 
 class CouponsCard extends Component {
+  splitDates = date => {
+    const UtcDate = new Date(date);
+    let month = UtcDate.getMonth();
+    const day = UtcDate.getDate() + 1;
+    const year = UtcDate.getFullYear();
+    const monthNames = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ];
+    month = monthNames[month];
+    const dateReturn = {
+      day,
+      month,
+      year
+    };
+    return dateReturn;
+  };
   render() {
+    const {
+      status,
+      code,
+      discountAmount,
+      // fromDate,
+      toDate,
+      subtotal,
+      discountValue,
+      type
+    } = this.props;
+    // const SplitedFromDate = this.splitDates(fromDate);
+    const SplitedToDate = this.splitDates(toDate);
     return (
       <Row sx={{ boxShadow: 'profile', border: 'divider' }} mb={20}>
         <Col width={8 / 10}>
           <Row p={20}>
-            <Image src={offerIcon} />
+            {/* <Image src={offerIcon} /> */}
+            <Text textAlign="center" color="textLight" variant="heading.medium">
+              {type === 'percent' ? <b>{discountValue} %</b> : <b>₹ {discountAmount}</b>}
+            </Text>
             <Box ml={15}>
-              <Text width={1} pb={5} variant="primary" fontFamily="light">
-                On minimum purchase of Rs. 500
-              </Text>
+              {subtotal && (
+                <Text width={1} pb={5} variant="primary" fontFamily="light">
+                  On minimum purchase of Rs. {subtotal}
+                </Text>
+              )}
               <Text width={1} pb={5} variant="primary" fontFamily="light">
                 Reason for Cancellation
               </Text>
-              <Text width={1} pb={5} variant="primary" fontFamily="medium">
-                Expired on 20th January 2020
-              </Text>
+              {toDate && (
+                <Text width={1} pb={5} variant="primary" fontFamily="medium">
+                  {status === 'available' ? 'Expiring by ' : 'Expired on '}
+                  {SplitedToDate.day} th {SplitedToDate.month} {SplitedToDate.year}
+                </Text>
+              )}
             </Box>
           </Row>
         </Col>
@@ -35,13 +83,34 @@ class CouponsCard extends Component {
           <Text textAlign="center" fontFamily="light" color="heading" variant="xSmall" pb={15}>
             COUPON CODE
           </Text>
-          <Text textAlign="center" color="textLight" variant="heading.medium">
-            HTOFF30
-          </Text>
+          {code && (
+            <Text textAlign="center" color="textLight" variant="heading.medium">
+              {code}
+            </Text>
+          )}
         </Col>
       </Row>
     );
   }
 }
-
+CouponsCard.defaultProps = {
+  status: '',
+  code: '',
+  discountAmount: '',
+  // fromDate: '',
+  toDate: '',
+  subtotal: '',
+  discountValue: '',
+  type: ''
+};
+CouponsCard.propTypes = {
+  status: PropTypes.string,
+  code: PropTypes.string,
+  discountAmount: PropTypes.string,
+  // fromDate: PropTypes.string,
+  toDate: PropTypes.string,
+  subtotal: PropTypes.string,
+  discountValue: PropTypes.string,
+  type: PropTypes.string
+};
 export default CouponsCard;
--- src/newComponents/Coupons/index.js
+++ src/newComponents/Coupons/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 /**
  * Components
@@ -8,8 +10,48 @@ import Row from 'hometown-components-dev/lib/RowHtV1';
 import Button from 'hometown-components-dev/lib/ButtonHtV1';
 import CouponsCard from './Card';
 
+@connect(({ profile }) => ({
+  profile: profile.data,
+  response: profile
+}))
 class CouponsContainer extends Component {
+  static propTypes = {
+    profile: PropTypes.shape({
+      coupons: PropTypes.object
+    })
+    // response: PropTypes.object
+  };
+  static defaultProps = {
+    profile: {}
+    // response: {},
+  };
+  state = {
+    coupons: {},
+    availableClicked: true
+  };
+  componentWillMount() {
+    const {
+      profile: { coupons }
+    } = this.props;
+    this.setState({
+      coupons
+    });
+  }
+  toggleButton = () => {
+    const { availableClicked } = this.state;
+    this.setState({
+      availableClicked: !availableClicked
+    });
+  };
   render() {
+    const { coupons, availableClicked } = this.state;
+    const { available, expired } = coupons;
+    const buttonSx = {
+      border: 'divider',
+      borderBottom: 'none',
+      top: 1,
+      position: 'relative'
+    };
     return (
       <Box width={[1, 1, 4 / 5]} px={10}>
         <Row
@@ -18,27 +60,54 @@ class CouponsContainer extends Component {
             borderBottom: 'divider'
           }}
         >
-          <Button width={120} height={40} variant="link" bg="white">
+          <Button
+            width={120}
+            height={40}
+            variant={availableClicked ? 'linkPrimary' : 'link'}
+            bg="white"
+            sx={availableClicked ? buttonSx : null}
+            onClick={this.toggleButton}
+          >
             ACTIVE
           </Button>
           <Button
             width={120}
             height={40}
-            variant="linkPrimary"
+            variant={!availableClicked ? 'linkPrimary' : 'link'}
             bg="white"
-            sx={{
-              border: 'divider',
-              borderBottom: 'none',
-              top: 1,
-              position: 'relative'
-            }}
+            sx={!availableClicked ? buttonSx : null}
+            onClick={this.toggleButton}
           >
             MISSED
           </Button>
         </Row>
-        <CouponsCard />
-        <CouponsCard />
-        <CouponsCard />
+        <Row>
+          {availableClicked
+            ? available.map(data => (
+              <CouponsCard
+                status="available"
+                code={data.code}
+                discountAmount={data.discount_amount}
+                fromDate={data.from_date}
+                toDate={data.to_date}
+                subtotal={data.subtotal}
+                discountValue={data.discount_value}
+                type={data.type}
+              />
+              ))
+            : expired.map(data => (
+              <CouponsCard
+                status="expired"
+                code={data.code}
+                discountAmount={data.discount_amount}
+                fromDate={data.from_date}
+                toDate={data.to_date}
+                subtotal={data.subtotal}
+                discountValue={data.discount_value}
+                type={data.type}
+              />
+              ))}
+        </Row>
       </Box>
     );
   }
