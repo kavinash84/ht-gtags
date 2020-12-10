f756c472591e573ce4e7cfaa21b5d96fdef1deb5
nikhil <nikhil@theretailinsights.com>
2020-12-10 16:16:39 +0530

    PDP small product image fix

--- src/components/ProductDetails/Carousel.js
+++ src/components/ProductDetails/Carousel.js
@@ -38,7 +38,7 @@ export default class ProductDetailSlider extends Component {
 
     return (
       <Row>
-        <Col>
+        <Col width="100%" className="custom-comp">
           <img className="hide" itemProp="image" src={(data && `${data[0].url}-zoom.jpg`) || ''} alt={title} />
           <Slider
             asNavFor={this.state.nav2}
