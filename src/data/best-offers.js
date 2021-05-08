16485c647d21adc4d716e267a6be246d9700d9ce
Nikhil Bhagath <nikhil@theretailinsights.com>
2021-05-08 17:43:21 +0530

    Listing best offer carosel

---
+++ src/data/best-offers.js
@@ -0,0 +1,45 @@
+const listingBestOffers = [
+  {
+    id: 1,
+    title: 'Best Offers',
+    description: 'Best Offers',
+    image: 'https://www.hometown.in/media/cms/icons/Bhushan/Holi-banner_1.jpg',
+    type: 'webview_layout',
+    meta: { name: 'Best Offers', id: 'Best Offers' }
+  },
+  {
+    id: 2,
+    title: 'Best Offers',
+    description: 'Best Offers',
+    image: 'https://www.hometown.in/media/cms/icons/Bhushan/exchange_&_upgrade_23.jpg',
+    type: 'webview_layout',
+    meta: { name: 'Best Offers', id: 'Best Offers' }
+  },
+  {
+    id: 3,
+    title: 'Best Offers',
+    description: 'Best Offers',
+    image: 'https://www.hometown.in/media/cms/icons/Bhushan/HT_Comes_home-Banner_4.jpg',
+    type: 'webview_layout',
+    meta: { name: 'Best Offers', id: 'Best Offers' }
+  },
+  {
+    id: 4,
+    title: 'Best Offers',
+    description: 'Best Offers',
+    image: 'https://www.hometown.in/media/cms/icons/Bhushan/HE_banner_1.jpg',
+    type: 'webview_layout',
+    meta: { name: 'Best Offers', id: 'Best Offers' }
+  },
+  {
+    id: 5,
+    title: 'Best Offers',
+    description: 'Best Offers',
+    image: 'https://www.hometown.in/media/cms/icons/Bhushan/HT_Comes_home-Banner_4.jpg',
+    type: 'webview_layout',
+    meta: { name: 'Best Offers', id: 'Best Offers' }
+  }
+];
+
+const listingBestOffersPath = ['/hot-deals/furniture-deals', '/hot-deals', '/home-furnishings/mothers-day'];
+export default { listingBestOffers, listingBestOffersPath };
