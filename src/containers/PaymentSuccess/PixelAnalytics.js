c978139dc48f34a26a440e49892bb8b2e03a175c
Nikhil Bhagath <nikhil@theretailinsights.com>
2021-07-29 13:57:56 +0530

    pixel code on payment success

---
+++ src/containers/PaymentSuccess/PixelAnalytics.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function PixelAnalytics({ transactionId, amount }) {
+  return (
+    <div>
+      {/* Offer Conversion: Hometown CPS */}
+      <img
+        src={`https://logicsd.go2cloud.org/aff_l?offer_id=18&adv_sub=${transactionId}&amount=${amount}`}
+        width="1"
+        height="1"
+        alt="pixel-analytics"
+      />
+      {/* End Offer Conversion */}
+    </div>
+  );
+}
+
+PixelAnalytics.defaultProps = {
+  transactionId: 0,
+  amount: 0
+};
+
+PixelAnalytics.propTypes = {
+  transactionId: PropTypes.number,
+  amount: PropTypes.number
+};
+
+export default PixelAnalytics;
