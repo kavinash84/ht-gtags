b51bc8db7933aa13c1df7e130884a51f801f0a62
yasmeen1999 <yasmeen@theretailinsights.com>
2021-11-22 00:16:33 +0530

    css changes

--- src/components/CategoryCarouselLook/CategoryCarouselItem.js
+++ src/components/CategoryCarouselLook/CategoryCarouselItem.js
@@ -1,45 +1,49 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 // import { Link } from "react-router-dom";
 
-import Img from "hometown-components-dev/lib/Img";
+import Img from 'hometown-components-dev/lib/Img';
 
 // import "./CategoryCarousel.css";
-const CategoryItem = ({ src, title }) => {
-  return (
-    <div>
+const CategoryItem = ({ src, title }) => (
+  <div>
+    <div
+      style={{
+          display: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+    >
       <div
         style={{
-          display: "flex",
-          flexDirection: "row",
-          flexWrap: "wrap",
-          justifyContent: "center",
-          alignItems: "center"
-        }}
-      >
-        <div
-          style={{
-            width: "22rem",
-            display: "flex",
-            height: "360px",
-            flexDirection: "column",
-            alignItems: "flex-start",
-            justifyContent: "flex-start"
+            width: '22rem',
+            display: 'flex',
+            height: '360px',
+            flexDirection: 'column',
+            alignItems: 'flex-start',
+            justifyContent: 'flex-start'
           }}
+      >
+        <Img src={src} m={5} height="auto" width="90%" style={{ zIndex: 10 }} />
+        <h3
+          style={{
+              fontSize: '16px',
+              fontWeight: '300',
+              margin: ' 20px 15px 0 5px',
+              wordSpacing: '2px',
+              letterSpacing: '0.6px',
+              lineHeight: '18px'
+            }}
+          className="get-the-look-titleOne"
         >
-          <Img
-            src={src}
-            m={5}
-            height="auto"
-            width="90%"
-            style={{ zIndex: 10 }}
-          />
-          <h3 className="get-the-look-title-one">{title}</h3>
-        </div>
+          {title}
+        </h3>
       </div>
     </div>
+  </div>
   );
-};
 
 CategoryItem.propTypes = {
   src: PropTypes.string.isRequired,
