f3dfd634ee690a6fb9f97a33a84bfba126aa3be4
Nikhil <mahaboob@theretailinsights.com>
2021-12-23 14:46:50 +0530

    bug fixes

--- src/components/Checkout/Emi.js
+++ src/components/Checkout/Emi.js
@@ -21,7 +21,8 @@ const mapStateToProps = ({ paymentoptions }) => ({
   selectedGateway: paymentoptions.selectedGateway,
   emiBankDetails: getEmiBanks(paymentoptions),
   emiDebitCardDetails: paymentoptions.data.paymentData.emiDebitCardDetails,
-  details: paymentoptions.paymentMethodDetails.Emi
+  details: paymentoptions.paymentMethodDetails.Emi,
+  isCreditSelected: paymentoptions.isCreditSelected
 });
 
 const onChangeDetails = (dispatcher, gateway) => e => {
@@ -35,12 +36,14 @@ const EMI = ({
   emiBankDetails,
   emiDebitCardDetails,
   details,
-  currentSelection
+  currentSelection,
+  isCreditSelected
 }) => {
   const currentBankDetails = emiBankDetails.filter(
     item => item.bank === details.emiBank
   )[0];
   const currentDebitBankDetails = Object.keys(emiDebitCardDetails);
+  const currentBank = details.emiBank;
   return (
     <Box>
       <Box pb={20} >
@@ -66,30 +69,7 @@ const EMI = ({
         ))}
       </Row>
 
-      <Box pb={20} mt="50px">
-        <Label>
-          Choose From Preferred Bank (Available On Debit Cards)
-        </Label>
-      </Box>
-      <Row pb={20}>
-        {currentDebitBankDetails.map(bank => (
-          <DebitBankCard
-            setPaymentDetails={setPaymentDetails}
-            gateway={selectedGateway}
-            detailkey="emiBank"
-            name={bank}
-            img={
-              ["citibank", "AMEX", "IndusInd", "Bob", "Yes"].includes(bank)
-                ? `https://www.hometown.in/media/cms/Bank/${bank}.jpeg`
-                : `https://static.hometown.in/media/cms/BankLOGO/${bank}.gif`
-            }
-            currentSelection={currentSelection}
-            key={bank}
-          />
-        ))}
-      </Row>
-
-      {currentBankDetails && (
+      {currentBankDetails && isCreditSelected && (
         <Box>
           <Box sx={{ overflow: "auto" }}>
             <Box
@@ -165,7 +145,116 @@ const EMI = ({
           </Box>
         </Box>
       )}
-    </Box>
+      
+      
+      <Box pb={20} mt="50px">
+        <Label>
+          Choose From Preferred Bank (Available On Debit Cards)
+        </Label>
+      </Box>
+      <Row pb={20}>
+        {currentDebitBankDetails.map(bank => (
+          <DebitBankCard
+            setPaymentDetails={setPaymentDetails}
+            gateway={selectedGateway}
+            detailkey="emiBank"
+            name={bank}
+            img={
+              ["citibank", "AMEX", "IndusInd", "Bob", "Yes"].includes(bank)
+                ? `https://www.hometown.in/media/cms/Bank/${bank}.jpeg`
+                : `https://static.hometown.in/media/cms/BankLOGO/${bank}.gif`
+            }
+            currentSelection={currentSelection}
+            key={bank}
+          />
+        ))}
+      </Row>
+      
+      {!isCreditSelected ? (
+      <Box>
+      {emiDebitCardDetails[currentBank] &&(
+        <Box>
+          <Box sx={{ overflow: "auto" }}>
+            <Box
+              as="table"
+              width={1}
+              mb={20}
+              sx={{
+                borderCollapse: "collapse",
+                border: "secondary",
+                "& tr": {
+                  border: "secondary"
+                },
+                "& td": {
+                  border: "secondary",
+                  p: "5px 8px",
+                  fontSize: 14
+                },
+                "& th": {
+                  border: "secondary",
+                  p: "10px 10px",
+                  fontSize: 14
+                }
+              }}
+            >
+              <tbody>
+                <tr>
+                  <th />
+                  <th width="85px">Tenure</th>
+                  <th>Annual Interest Rate</th>
+                  <th>EMI Interest</th>
+                  <th>Total Cost</th>
+                  <th>Monthly Instalments</th>
+                </tr>
+
+                {emiDebitCardDetails[currentBank] &&
+                 emiDebitCardDetails[currentBank].emiOptions &&
+                 Object.values(
+                  emiDebitCardDetails[currentBank].emiOptions
+                  ).map((item, index) => (
+                  <tr key={String(index)}>
+                    <td align="center">
+                      <input
+                        type="radio"
+                        onChange={onChangeDetails(
+                          setPaymentDetails,
+                          selectedGateway
+                        )}
+                        name="emiCode"
+                        value={item.emiCode}
+                      />
+                    </td>
+                    <td>{item.value} Months</td>
+                    <td>{item.interestRate}%</td>
+                    <td>
+                      Rs.
+                      {Math.round(item.emiInterest)}
+                    </td>
+                    <td>
+                      Rs.
+                      {Math.round(item.totalAmount)}
+                    </td>
+                    <td>
+                      Rs.
+                      {Math.round(item.EMI)}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Box>
+          </Box>
+          <Box>
+            <CardForm
+              setPaymentDetails={setPaymentDetails}
+              gateway={selectedGateway}
+              padding="1rem 0rem 0"
+            />
+          </Box>
+        </Box>
+      )}
+      </Box>
+      ) : null}
+      </Box>
   );
 };
 
