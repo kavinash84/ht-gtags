62185c3e0c01858882aabc43c555b31d257d7c81
Nikhil <mahaboob@theretailinsights.com>
2022-01-03 11:48:35 +0530

    unbxd id added

--- src/containers/Listing/Listing.js
+++ src/containers/Listing/Listing.js
@@ -394,6 +394,11 @@ export default class Listing extends Component {
                 bannerData={bannerData}
               />
             </div>
+
+            <div>
+            <div id="unbxd_category_top_sellers"></div>
+            </div>
+            
             {seoInfo && seoInfo.seo_text && (
               <SeoContent>
                 <div dangerouslySetInnerHTML={{ __html: seoInfo.seo_text }} />
