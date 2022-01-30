58f054f21f0389e14c85c6e878532cf19f5141b4
veeresh <veeresh@theretailinsights.com>
2022-01-30 13:37:20 +0530

    added product details drop down

--- src/components/ProductDetails/EmiOptions.js
+++ src/components/ProductDetails/EmiOptions.js
@@ -1,22 +1,22 @@
 /* eslint-disable max-len */
-import React from 'react';
-import PropTypes from 'prop-types';
-// import Div from 'hometown-components-dev/lib/BoxHtV1';
+import React from "react";
+import PropTypes from "prop-types";
+import Div from "hometown-components-dev/lib/BoxHtV1";
 // import Flex from 'hometown-components-dev/lib/FlexHtV1';
 // import Image from 'hometown-components-dev/lib/ImageHtV1';
-// import Text from 'hometown-components-dev/lib/TextHtV1';
-// import Heading from 'hometown-components-dev/lib/HeadingHtV1';
-import { Link } from 'react-router-dom';
-import Div from 'hometown-components/lib/Div';
+import Text from "hometown-components-dev/lib/TextHtV1";
+import Heading from "hometown-components-dev/lib/HeadingHtV1";
+import { Link } from "react-router-dom";
+// import Div from 'hometown-components/lib/Div';
 // import Flex from 'hometown-components/lib/Row';
 // import Image from 'hometown-components/lib/Img';
-import Text from 'hometown-components/lib/Text';
-import Heading from 'hometown-components/lib/Heading';
-import SlickSlider from '../SlickSlider';
+// import Text from 'hometown-components/lib/Text';
+// import Heading from 'hometown-components/lib/Heading';
+import SlickSlider from "../SlickSlider";
 
 // import Slider from './Slider-copy';
 
-const styles = require('./EmiOptions.scss');
+const styles = require("./EmiOptions.scss");
 
 const adjustSlides = length => ({
   slidesToShow: length >= 2 ? 1.4 : length,
@@ -43,18 +43,23 @@ function HtExclusiveTemplate4({ data }) {
                 collection.map((slide, index) => (
                   <Div key={String(index)} p="10px 0px" pr="10px">
                     <Div className={styles.paymentOptions}>
-                    <Link to="/promotions">
-                      {/* <Div pt={'20px'} pb={'10px'}>
+                      <Link to="/promotions">
+                        {/* <Div pt={'20px'} pb={'10px'}>
                         <Image width="110px" src={slide.url} alt="bank-option" />
                       </Div> */}
-                      <Div>
-                        <Text fontSize="11px" style={{fontWeight:"bold"}} mt="0" mb="0.3rem">
-                          {slide.title}
-                        </Text>
-                        <Text fontSize="11px" mt="0" mb="0">
-                          {slide.desc}
-                        </Text>
-                      </Div>
+                        <Div>
+                          <Text
+                            fontSize="11px"
+                            style={{ fontWeight: "bold" }}
+                            mt="0"
+                            mb="0.3rem"
+                          >
+                            {slide.title}
+                          </Text>
+                          <Text fontSize="11px" mt="0" mb="0">
+                            {slide.desc}
+                          </Text>
+                        </Div>
                       </Link>
                     </Div>
                   </Div>
@@ -72,10 +77,10 @@ HtExclusiveTemplate4.propTypes = {
 
 HtExclusiveTemplate4.defaultProps = {
   data: {
-    mainTitle: '',
-    subTitles: '',
+    mainTitle: "",
+    subTitles: "",
     imageCollection: [],
-    banner: ''
+    banner: ""
   }
 };
 
