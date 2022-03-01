50092b37472ccdcc76edc06ebedadecad5d9275a
yasmeen1999 <yasmeen@theretailinsights.com>
2022-03-01 12:10:42 +0530

    specs alignment for description

--- src/components/ProductDetails/Specs/spec.js
+++ src/components/ProductDetails/Specs/spec.js
@@ -26,8 +26,8 @@ class Spec extends React.Component {
 
     return (
       <Row display="block" m="0" pb="2px">
-        {label === "Note" ? (
-          <div style={{ fontStyle: "normal" , marginTop:"12px"}}>
+        {label === "Note" || label === "Short Description" ? (
+          <div style={{ fontStyle: "normal", marginTop: "12px" }}>
             <div>
               {label !== "Care Instructions" &&
                 label !== "Returns / Cancellation" &&
@@ -47,9 +47,9 @@ class Spec extends React.Component {
               <Div
                 col={
                   label === "Care Instructions" ||
-                  label === "Note" ||
-                  label === "Service Assurance / Warranty" ||
-                  label === "Returns / Cancellation"
+                    label === "Note" ||
+                    label === "Service Assurance / Warranty" ||
+                    label === "Returns / Cancellation"
                     ? "12"
                     : "6"
                 }
@@ -63,77 +63,78 @@ class Spec extends React.Component {
                   lh="1.8"
                   color="rgba(0, 0, 0, 0.65)"
                   fontFamily="light"
+                  style={{ lineHeight: "25px" }}
                 />
               </Div>
             </div>
           </div>
         ) : (
-          <div
-            style={{
-              fontStyle: "normal",
-              display: "flex",
-              width: "100%",
-              marginTop: "8px"
-            }}
-          >
-          {label === "Care Instructions" || label === "Returns / Cancellation" || label === "Service Assurance / Warranty" ? (
-            <div>
-              {label !== "Care Instructions" &&
-                label !== "Returns / Cancellation" &&
-                label !== "Service Assurance / Warranty" && (
-                  <Div col="6">
-                    <Label
-                      lh="1.6"
-                      fontFamily="regular"
-                      color=" rgba(0, 0, 0, 0.65)"
-                    >
-                      {label}
-                    </Label>
-                  </Div>
+            <div
+              style={{
+                fontStyle: "normal",
+                display: "flex",
+                width: "100%",
+                marginTop: "8px"
+              }}
+            >
+              {label === "Care Instructions" || label === "Returns / Cancellation" || label === "Service Assurance / Warranty" ? (
+                <div>
+                  {label !== "Care Instructions" &&
+                    label !== "Returns / Cancellation" &&
+                    label !== "Service Assurance / Warranty" && (
+                      <Div col="6">
+                        <Label
+                          lh="1.6"
+                          fontFamily="regular"
+                          color=" rgba(0, 0, 0, 0.65)"
+                        >
+                          {label}
+                        </Label>
+                      </Div>
+                    )}
+                </div>
+              ) : (
+                  <div style={{ width: "50%" }}>
+                    {label !== "Care Instructions" &&
+                      label !== "Returns / Cancellation" &&
+                      label !== "Service Assurance / Warranty" && (
+                        <Div col="6">
+                          <Label
+                            lh="1.6"
+                            fontFamily="regular"
+                            color=" rgba(0, 0, 0, 0.65)"
+                          >
+                            {label}
+                          </Label>
+                        </Div>
+                      )}
+                  </div>
                 )}
-            </div>
-          ):(
-            <div style={{ width: "50%" }}>
-              {label !== "Care Instructions" &&
-                label !== "Returns / Cancellation" &&
-                label !== "Service Assurance / Warranty" && (
-                  <Div col="6">
-                    <Label
-                      lh="1.6"
-                      fontFamily="regular"
-                      color=" rgba(0, 0, 0, 0.65)"
-                    >
-                      {label}
-                    </Label>
-                  </Div>
-                )}
+
+              <div>
+                <Div
+                  col={
+                    label === "Care Instructions" ||
+                      label === "Note" ||
+                      label === "Service Assurance / Warranty" ||
+                      label === "Returns / Cancellation"
+                      ? "12"
+                      : "6"
+                  }
+                >
+                  <Description
+                    mb="7px"
+                    itemProp="description"
+                    fontSize="0.875rem"
+                    dangerouslySetInnerHTML={{ __html: value }}
+                    lh="1.8"
+                    color="rgba(0, 0, 0, 0.65)"
+                    fontFamily="light"
+                  />
+                </Div>
+              </div>
             </div>
           )}
-            
-            <div>
-              <Div
-                col={
-                  label === "Care Instructions" ||
-                  label === "Note" ||
-                  label === "Service Assurance / Warranty" ||
-                  label === "Returns / Cancellation"
-                    ? "12"
-                    : "6"
-                }
-              >
-                <Description
-                  mb="7px"
-                  itemProp="description"
-                  fontSize="0.875rem"
-                  dangerouslySetInnerHTML={{ __html: value }}
-                  lh="1.8"
-                  color="rgba(0, 0, 0, 0.65)"
-                  fontFamily="light"
-                />
-              </Div>
-            </div>
-          </div>
-        )}
       </Row>
     );
   }
