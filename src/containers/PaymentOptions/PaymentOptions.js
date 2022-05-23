ede8afb7d02fa64dc666d7f2439ab5e58fe842ea
veeresh <veeresh@theretailinsights.com>
2022-05-23 11:18:46 +0530

    made changes to payment error

--- src/containers/PaymentOptions/PaymentOptions.js
+++ src/containers/PaymentOptions/PaymentOptions.js
@@ -1,32 +1,33 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Helmet from 'react-helmet';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import { connect } from "react-redux";
+import { withRouter } from "react-router";
 
 /**
  * Components
  */
-import Body from 'hometown-components-dev/lib/BodyHtV1';
-import Wrapper from 'hometown-components-dev/lib/WrapperHtV1';
+import Body from "hometown-components-dev/lib/BodyHtV1";
+import Wrapper from "hometown-components-dev/lib/WrapperHtV1";
 
 /**
  * Page Components
  */
-import PaymentOptions from 'components/Checkout/PaymentOptions';
-import HeaderSecondary from 'components/HeaderSecondary';
-import CheckoutBreadCumb from 'components/Checkout/breadDumb';
+import PaymentOptions from "components/Checkout/PaymentOptions";
+import HeaderSecondary from "components/HeaderSecondary";
+import CheckoutBreadCumb from "components/Checkout/breadDumb";
 
 /**
  * modules / selectors / helpers
  */
-import { CART_URL } from 'helpers/Constants';
-import { getPaymentOptions } from 'selectors/payments';
-import { getCartList } from 'selectors/cart';
+import { CART_URL } from "helpers/Constants";
+import { getPaymentOptions } from "selectors/payments";
+import { getCartList } from "selectors/cart";
 
 @withRouter
 @connect(({ paymentoptions, cart, address: { shipping } }) => ({
   availableOptions: getPaymentOptions(paymentoptions),
+  paymentOptionsError: paymentoptions.error,
   cart: getCartList(cart),
   shipping
 }))
@@ -37,8 +38,22 @@ export default class PaymentOptionsContainer extends Component {
       history,
       shipping: { pincode, fullName, phone }
     } = this.props;
-    if ((cart && cart.length === 0) || pincode === '' || fullName === '' || phone === '') {
+    if (
+      (cart && cart.length === 0) ||
+      pincode === "" ||
+      fullName === "" ||
+      phone === ""
+    ) {
       history.push(CART_URL);
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    const { paymentOptionsError, history } = this.props;
+    if (prevProps.paymentOptionsError !== paymentOptionsError) {
+      if (paymentOptionsError === "Cart total mismatch") {
+        console.log(paymentOptionsError, "paymentOptionsError");
+        history.push("/");
+      }
     }
   }
   render() {
@@ -62,9 +77,9 @@ PaymentOptionsContainer.defaultProps = {
   cart: [],
   history: {},
   shipping: {
-    fullName: '',
-    phone: '',
-    pincode: ''
+    fullName: "",
+    phone: "",
+    pincode: ""
   }
 };
 
