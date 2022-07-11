ee740157193fcd86fcd52d7364133efd45b73ebd
veeresh <veeresh@theretailinsights.com>
2022-07-11 15:50:40 +0530

    made changes recs v2

--- src/components/NewUnboxWidges/bestSeller.js
+++ src/components/NewUnboxWidges/bestSeller.js
@@ -9,6 +9,49 @@ export default class NewUnboxBestSeller extends Component {
   componentDidMount() {
     if (window && window._unbxd_getRecommendations) {
       const { session, pageInfo } = this.props;
+      beforeTemplateRenderer = function(templateData) {
+        var modifyTemplateData = function(data) {
+          if (!data || !data.recommendations) {
+            return;
+          }
+
+          var sliderWidth = jQuery("._unbxd_recs-slider-container").width();
+          var sliderOffset = 10;
+          var productTileGap = 50;
+
+          var recsBoxSize = sliderWidth / 4 - sliderOffset - productTileGap;
+
+          var getImageSuffix = function() {
+            var imageSuffix =
+              ".jpg?" +
+              "width=" +
+              recsBoxSize +
+              "&height=" +
+              recsBoxSize +
+              "&mode=fill";
+            return imageSuffix;
+          };
+
+          var updatedRecsData = data.recommendations.map(function(product) {
+            var imageURL = product.imageURL + getImageSuffix();
+            var recsBoxWidth = recsBoxSize + "px";
+            var recsBoxHeight = recsBoxSize + "px";
+
+            return { ...product, imageURL, recsBoxWidth, recsBoxHeight };
+          });
+
+          return { ...data, recommendations: updatedRecsData };
+        };
+
+        templateData = modifyTemplateData(templateData);
+
+        return templateData;
+      };
+
+      window._unbxd_registerHook(
+        "beforeTemplateRender",
+        beforeTemplateRenderer
+      );
       window._unbxd_getRecommendations({
         widgets: {
           widget1: {
