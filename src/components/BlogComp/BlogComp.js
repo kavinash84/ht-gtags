3c629d58f5f57eb9db02b90767cab649bdf69f5b
veeresh <veeresh@theretailinsights.com>
2022-07-26 17:15:10 +0530

    working on blog

--- src/components/BlogComp/BlogComp.js
+++ src/components/BlogComp/BlogComp.js
@@ -1,76 +1,112 @@
 import React from "react";
 import image from "./images/img.png";
 import image2 from "./images/rec303.png";
-import "./blogcomp.scss";
+import PackageBreadCrumb from "./blogsBreadcrumb";
+
+const BreadCrumpstyles = require("./breadcrumb.scss");
+const styles = require("./index.scss");
 
 class BlogComp extends React.Component {
+  state = { activeTab: 0 };
+
+  handleTabChange = id => {
+    this.setState({ activeTab: id });
+  };
+
   render() {
+    const { activeTab } = this.state;
     return (
-      <div className="blogs_main">
-        <img className="banner" src={image} />
+      <div className={styles.blogs_main}>
+        <div className={BreadCrumpstyles.BreadCrumb_wrapper2}>
+          <PackageBreadCrumb />
+        </div>
+        <img className={styles.banner} src={image} />
 
-        <div className="lists">
-          <ul>
-            <li>Design and Decorate</li>
-            <li>Organization</li>
-            <li>How-to-guides</li>
-            <li>LifeStyle</li>
+        <div className={styles.cataagoryContainer}>
+          <ul className={styles.catagoryList}>
+            <li
+              className={activeTab === 0 ? "activeTab" : ""}
+              onClick={() => this.handleTabChange(0)}
+            >
+              Design and Decorate
+            </li>
+            <li
+              className={activeTab === 1 ? "activeTab" : ""}
+              onClick={() => this.handleTabChange(1)}
+            >
+              Organization
+            </li>
+            <li
+              className={activeTab === 2 ? "activeTab" : ""}
+              onClick={() => this.handleTabChange(2)}
+            >
+              How-to-guides
+            </li>
+            <li
+              className={activeTab === 3 ? "activeTab" : ""}
+              onClick={() => this.handleTabChange(3)}
+            >
+              LifeStyle
+            </li>
           </ul>
-        </div>
 
-        <div className="images">
-          <div>
-            <img src={image2} className="img1"></img>
-            <h5>
-              Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit,
-            </h5>
-            <br />
-            <h6>12th January 2022</h6>
-          </div>
-          <div>
-            <img src={image2} className="img1"></img>
-            <h5>
-              Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit,
-            </h5>
-            <br />
-            <h6>12th January 2022</h6>
-          </div>
-          <div>
-            <img src={image2} className="img1"></img>
-            <h5>
-              Lorem ipsum dolor sit amet, <br />
-              consectetur adipiscing elit,
-            </h5>
-            <br />
-            <h6>12th January 2022</h6>
-          </div>
-        </div>
-
-        <div className="images2">
-          <div>
-            <img src={image2} className="img1"></img>
-            <h5>
-              Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit,
-            </h5>
-            <br />
-            <h6>12th January 2022</h6>
-          </div>
-          <div>
-            <img src={image2} className="img1"></img>
-            <h5>
-              Lorem ipsum dolor sit amet, <br /> consectetur adipiscing elit,
-            </h5>
-            <br />
-            <h6>12th January 2022</h6>
-          </div>
-          <div>
-            <img src={image2} className="img1"></img>
-            <h5>
-              Lorem ipsum dolor sit amet, <br />
-              consectetur adipiscing elit,
-            </h5>
-            <br />
-            <h6>12th January 2022</h6>
+          <div className={styles.posts}>
+            <div className={styles.post}>
+              <img className={styles.image} src={image2} />
+              <div className={styles.title}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              </div>
+              <div className={styles.date}>12th January 2022</div>
+            </div>
+            <div className={styles.post}>
+              <img className={styles.image} src={image2} />
+              <div className={styles.title}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              </div>
+              <div className={styles.date}>12th January 2022</div>
+            </div>
+            <div className={styles.post}>
+              <img className={styles.image} src={image2} />
+              <div className={styles.title}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              </div>
+              <div className={styles.date}>12th January 2022</div>
+            </div>
+            <div className={styles.post}>
+              <img className={styles.image} src={image2} />
+              <div className={styles.title}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              </div>
+              <div className={styles.date}>12th January 2022</div>
+            </div>
+            <div className={styles.post}>
+              <img className={styles.image} src={image2} />
+              <div className={styles.title}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              </div>
+              <div className={styles.date}>12th January 2022</div>
+            </div>
+            <div className={styles.post}>
+              <img className={styles.image} src={image2} />
+              <div className={styles.title}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              </div>
+              <div className={styles.date}>12th January 2022</div>
+            </div>
+            <div className={styles.post}>
+              <img className={styles.image} src={image2} />
+              <div className={styles.title}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              </div>
+              <div className={styles.date}>12th January 2022</div>
+            </div>
+            <div className={styles.post}>
+              <img className={styles.image} src={image2} />
+              <div className={styles.title}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              </div>
+              <div className={styles.date}>12th January 2022</div>
+            </div>
           </div>
         </div>
       </div>
