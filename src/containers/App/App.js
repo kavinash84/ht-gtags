3a3fb1c8c8cbb4088d57661ffcf009ca0668faf7
Pasha <mahaboob@theretailinsights.com>
2022-08-16 10:09:31 +0530

    added lazyload

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -356,7 +356,8 @@ export default class App extends Component {
         hosts: [{
             current: "www.hometown.in",
             gumlet: "hometown.gumlet.io"
-        }]
+        }],
+        lazy_load: true
     };
     (function(){d=document;s=d.createElement("script");s.src="https://cdn.gumlet.com/gumlet.js/2.1/gumlet.min.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
     `}
