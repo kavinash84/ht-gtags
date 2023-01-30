e35d188a2e3acc105f2e4c1927e84090b1e0a48f
Yogesh Kumar <yogesh@hometown.in>
2023-01-30 10:15:40 +0530

    exchange offer code uncommented

--- src/containers/Exchange-offer/exhangeOfferContainer.js
+++ src/containers/Exchange-offer/exhangeOfferContainer.js
@@ -19,13 +19,7 @@ export default class ExchangeOfferContainer extends Component {
 
   componentDidMount() {
     const { history } = this.props;
-    const { dispatch } = this.context.store;
     history.push('/');
-    dispatch(notifSend({
-        type: 'warning',
-        msg: 'This offer has been expired.',
-        dismissAfter: 4000
-      }));
   }
 
   render() {
