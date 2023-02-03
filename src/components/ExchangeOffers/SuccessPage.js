b17799d22a82e576489c5e225f227a21ade9c93e
Yogesh Kumar <yogesh@hometown.in>
2023-02-03 14:56:37 +0530

    fb track event removed

--- src/components/ExchangeOffers/SuccessPage.js
+++ src/components/ExchangeOffers/SuccessPage.js
@@ -83,24 +83,8 @@ class SuccessPage extends Component {
              s.parentNode.insertBefore(t,s)}(window, document,'script',
              'https://connect.facebook.net/en_US/fbevents.js');
              fbq('init', '1024172491523922');
-             fbq('track', 'Lead'); 
+             fbq('track', 'Lead');
             `}
-          </script>
-
-          {/* Meta Pixel Code  */}
-          <script>
-            {`
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1024172491523922');
-fbq('track', 'Lead');
-`}
           </script>
           <noscript>
             {`<img height="1" width="1" style="display:none"
