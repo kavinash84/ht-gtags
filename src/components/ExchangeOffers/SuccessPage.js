d883bc730082ee8483df8edf70a2e59161f6b084
Pasha <mahaboob@theretailinsights.com>
2023-02-04 08:46:46 +0530

    test

--- src/components/ExchangeOffers/SuccessPage.js
+++ src/components/ExchangeOffers/SuccessPage.js
@@ -61,6 +61,20 @@ class SuccessPage extends Component {
           <script>
             {` gtag('event', 'conversion', {'send_to': 'AW-832074530/j9-7CM6CpqQDEKLm4YwD'}); `}
           </script>
+           <script type="text/javascript">
+            {`
+             !function(f,b,e,v,n,t,s)
+             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+             n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+             if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+             n.queue=[];t=b.createElement(e);t.async=!0;
+             t.src=v;s=b.getElementsByTagName(e)[0];
+             s.parentNode.insertBefore(t,s)}(window, document,'script',
+             'https://connect.facebook.net/en_US/fbevents.js');
+             fbq('init', '1024172491523922');
+             fbq('track', 'Lead');
+            `}
+          </script>
           
           {/* <!-- End Meta Pixel Code --> */}
         </Helmet>
